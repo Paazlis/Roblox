ce6ddfee5a6ed74098fbe8f7e9837f53aa6fa862
Paazlis <paazlismaswa@gmail.com>
2026-06-01 15:34:04 +0700

    Example Mercury V10

--- Packages/Mercury/ExampleMercury.lua
+++ Packages/Mercury/ExampleMercury.lua
@@ -1,16 +1,5 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/PaazlisMaswa/RobloxProject/refs/heads/main/Packages/Mercury/ExampleMercury.lua"))()
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/PaazlisMaswa/RobloxProject/refs/heads/main/Packages/Mercury/init.luau"))()
-
-local Gui = Library:Create({
-    Theme = Library.Themes.Serika
-})
-
-local Tab = Gui:Tab({
-    Icon = "rbxassetid://6034996695",
-    Name = "Aimbot"
-})
-
-local Label = Tab:Label({
-    Name = "Selemat datang di aimbot",
-    Description = "Kegunaan untuk mentarget yang terdekat pemain"
-})
+local Window = Library:Create({Theme = Library.Themes.Serika})
+local Tab = Window:Tab({Icon = "rbxassetid://6034996695",Name = "Aimbot"})
+local Label = Tab:Label({Name = "Selemat datang di aimbot",Description = "Kegunaan untuk mentarget yang terdekat pemain"})

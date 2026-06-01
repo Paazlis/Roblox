200af4e78d5360cc6d58fad4dccdc21712804b9e
Paazlis <paazlismaswa@gmail.com>
2026-06-01 13:37:49 +0700

    Update ExampleMercury.lua

--- Packages/Mercury/ExampleMercury.lua
+++ Packages/Mercury/ExampleMercury.lua
@@ -1,39 +1,39 @@
--- loadstring(game:HttpGet('https://raw.githubusercontent.com/PaazlisMaswa/RobloxProject/refs/heads/main/Packages/Mercury/ExampleMercury.lua'))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/PaazlisMaswa/RobloxProject/refs/heads/main/Packages/Mercury/ExampleMercury.lua"))()
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/PaazlisMaswa/RobloxProject/refs/heads/main/Packages/Mercury/init.luau"))()
 
-local gui = Library:create({
+local Window = Library:Create({
     Theme = Library.Themes.Serika
 })
 
-local Tab = Window:tab({
+local Tab = Window:Tab({
     Icon = "rbxassetid://6034996695",
     Name = "Aimbot"
 }
 
-Tab:button({
-    Name = "show prompt",
+Tab:Button({
+    Name = "Show Prompt",
     Callback = function()
-        tab:prompt{
+        tab:Prompt{
             Title = "baby",
             Text = "nice to meet you",
             Buttons = {
                 Ok = function()
-                    tab:prompt{
+                    Tab:Prompt{
                         Followup = true,
                         Title = "really?",
                         Text = "you sure?=",
                         Buttons = {
                             Yes = function()
-                                tab:Prompt{
+                                Tab:Prompt{
                                     Followup = true,
                                     Title = "lol",
                                     Text = "Welcome",
                                     Buttons = {
                                         Learn = function()
-                                            gui:set_status("money")
+                                            Window:set_status("money")
                                         end,
                                         Lazy = function()
-                                            gui:set_Status("homeless")
+                                            Window:set_Status("homeless")
                                         end
                                     }
                                 }
@@ -46,8 +46,8 @@ Tab:button({
     end
 })
 
-Tab:keybind({Callback = function()
-    gui:prompt()
+Tab:Keybind({Callback = function()
+    Window:Prompt()
 end})
 
 Tab:Dropdown({
@@ -61,17 +61,17 @@ Tab:Dropdown({
     }
 })
 
-Tab:slider({
+Tab:Slider({
     Name = "Slider",
     Description = "Slider Description",
     StartingText = "Aimbot Speed",
     Callback = function(v)
-       gui:set_status(v)
+       Window:set_status(v)
     end
 })
 
 Tab:Textbox({Callback = function(v)
-    gui:Prompt{Text = v}
+    Window:Prompt{Text = v}
 end,})
 
 Tab:color_picker({
@@ -82,5 +82,3 @@ Tab:color_picker({
         print(color)
     end,
 })
-
-Tab:Toggle({})

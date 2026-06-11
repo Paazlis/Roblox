f14e9fb29f45509185abbb150de282075be5e727
Paazlis <paazlismaswa@gmail.com>
2026-06-11 20:19:33 +0700

    Create Merge a Nuke

--- Mods/Games/MergeANuke.lua
+++ Mods/Games/MergeANuke.lua
@@ -1,96 +1,142 @@
 local UI=loadstring(game:HttpGet("http://raw.githubusercontent.com/Paazlis/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
 
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+local Players=game:GetService("Players")
+local LocalPlayer=Players.LocalPlayer
 
-local Bases = workspace.Bases
+local Bases=workspace:FindFirstChild("Bases")
 
-getgenv().CollectNukeEnabled = false
+getgenv().CollectNuke = false
 
 local Window=UI:CreateWindow({Name="Merge a Nuke"})
 
 Window:AddToggle({Name="Collect Nuke", Callback=function(value)
-     getgenv().CollectNukeEnabled = value
+	getgenv().CollectNuke = value
 end})
-      
+
 local function getMyBase()
- local allBases = basesFolder:GetChildren()
- for _, base in ipairs(allBases) do
-  local ownerId = base:GetAttribute("OwnerUserId")
-  if ownerId == localPlayer.UserId then
-   return base
-  end
- end
-   return nil
+	local allBases=Bases:GetChildren()
+	for _, base in ipairs(allBases) do
+		local ownerId=base:GetAttribute("OwnerUserId")
+		if ownerId==LocalPlayer.UserId then
+			return base
+		end
+	end
+	return nil
 end
 
-task.spawn(function()
-    while true do
-       task.wait(0.5)
-       if not getgenv().CollectNukeEnabled then continue end
-
-       local myBase = getMyBase()
+local function getNukes(base)
+	local nukes={}
+	local nukeFolder=base:FindFirstChild("Nukes")
 	
-	   if myBase then
-		  local validNukes = {}
-		  local nukes = myBase:FindFirstChild("Nukes")
-		  if not nukes then continue end
-		
-		  for _, nuke in ipairs(nukes:GetChildren()) do
-			local tier = tonumber(object:GetAttribute("Tier") or math.huge)
-			if tier ~= nil then
-				table.insert(validNukes, nuke)
-			end
-		   end
-		
-		   table.sort(validNukes, function(a, b)
-			  return tonumber(a:GetAttribute("Tier")) < tonumber(b:GetAttribute("Tier"))
-		   end)
-		
-		   local character = localPlayer.Character
-	    	local humanoid = character and character:FindFirstChild("Humanoid")
-		   local hrp = character and character:FindFirstChild("HumanoidRootPart")
-		
-		   if humanoid and hrp then
-			  for _, nuke in ipairs(validNukes) do
-				if not getgenv().CollectNukeEnabled then break end
-				
-				local nukeTier = tonumber(nuke:GetAttribute("Tier"))
-				local heldNukeVisual = camera:FindFirstChild("HeldNukeVisual")
-				
-				if heldNukeVisual then
-					local heldTier = tonumber(heldNukeVisual:GetAttribute("Tier"))
-					if heldTier and nukeTier == heldTier then
+	if nukeFolder then
+		for _,nuke in ipairs(nukeFolder:GetChildren()) do
+			local tier=tonumber(nuke:GetAttribute("Tier"))
+			if tier~=nil then
+				local pickNuke=workspace.CurrentCamera:FindFirstChild("HeldNukeVisual")
+				if pickNuke then
+					local pickTier=tonumber(pickNuke:GetAttribute("Tier"))
+					if pickTier~=nil and tier~=pickTier then
 						continue
 					end
 				end
-				
-				local targetPosition = nil
-				if nuke:IsA("BasePart") then
-					targetPosition = nuke.Position
-				elseif nuke:IsA("Model") and nuke.PrimaryPart then
-					targetPosition = nuke.PrimaryPart.Position
+				table.insert(nukes,nuke)
+			end
+		end
+
+		table.sort(nukes,function(a, b)
+			return tonumber(a:GetAttribute("Tier"))<tonumber(b:GetAttribute("Tier"))
+		end)
+		
+		local warnings={}
+		for i,v in ipairs(nukes) do
+			table.insert(warnings,v:GetAttribute("Tier"))
+		end
+		warn(warnings)
+	end
+	
+	return nukes
+end
+
+local LocalCamera=workspace.CurrentCamera
+local PickNuke=nil
+local LocalCameraAdded,LocalCameraRemoved=nil,nil
+
+local setupNuke=function()
+	LocalCamera=workspace.CurrentCamera
+	
+	if LocalCameraAdded then LocalCameraAdded:Disconnect() end
+	LocalCameraAdded=LocalCamera.ChildAdded:Connect(function(child)
+		if child.Name=="HeldNukeVisual" then
+			local pickTier=tonumber(child:GetAttribute("Tier"))
+			if pickTier~=nil then
+				PickNuke=child
+			end
+		end
+	end)
+	if LocalCameraRemoved then LocalCameraRemoved:Disconnect() end
+	LocalCameraRemoved=LocalCamera.ChildRemoved:Connect(function(child)
+		if PickNuke==child then PickNuke=nil end
+	end)
+end
+
+setupNuke()
+workspace:GetPropertyChangedSignal("CurrentCamera"):Connect(setupNuke)
+
+task.spawn(function()
+	while true do
+		task.wait(0.5)
+		if not getgenv().CollectNuke then continue end
+
+		local base=getMyBase()
+		if not base then continue end
+		
+		local nukes=getNukes(base)
+		if #nukes<=0 then continue end
+		
+		local character=LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+		local humanoid=character and character:FindFirstChildOfClass("Humanoid")
+		local hrp=character and (character:FindFirstChild("HumanoidRootPart") or character.PrimaryPart)
+		
+		if not humanoid or not hrp then continue end
+		
+		for _,nuke in ipairs(nukes) do
+			if not getgenv().CollectNuke then continue end
+			if not (nuke and nuke.Parent) then continue end
+
+			local tier=tonumber(nuke:GetAttribute("Tier"))
+			
+			if PickNuke and PickNuke.Parent and tier~=nil then
+				local PickTier=tonumber(PickNuke:GetAttribute("Tier"))
+				if PickTier~=nil and tier~=PickTier then
+					continue
 				end
+			end
+
+			local targetPosition=nil
+			if nuke:IsA("BasePart") then
+				targetPosition=nuke.Position
+			elseif nuke:IsA("Model") and nuke.PrimaryPart then
+				targetPosition=nuke.PrimaryPart.Position
+			end
+
+			if targetPosition and nuke.Parent then
+				humanoid:MoveTo(targetPosition)
 				
-				if targetPosition and nuke:IsDescendantOf(workspace) then
-					humanoid:MoveTo(targetPosition)
+				warn("Target is Nuke Tier "..tostring(tier))
+				
+				while (hrp.Position-targetPosition).Magnitude>4 and nuke.Parent and getgenv().CollectNuke do
+					task.wait(0.1)
 					
-					while (hrp.Position - targetPosition).Magnitude > 4 and nuke:IsDescendantOf(workspace) and getgenv().CollectNukeEnabled do
-						task.wait(0.1)
-						
-						local currentHeld = camera:FindFirstChild("HeldNukeVisual")
-						if currentHeld then
-							local currentHeldTier = tonumber(currentHeld:GetAttribute("Tier"))
-							if currentHeldTier and nukeTier == currentHeldTier then
-								break
-							end
+					if PickNuke and PickNuke.Parent and tier~=nil then
+						local PickTier=tonumber(PickNuke:GetAttribute("Tier"))
+						if PickTier~=nil and tier~=PickTier then
+							break
 						end
-						
-						humanoid:MoveTo(targetPosition)
 					end
+					
+					humanoid:MoveTo(targetPosition)
 				end
-			  end
-		   end
-	   end
-    end
+			end
+		end
+	end
 end)

2fcc929854493507789804eaa9de7c1f13b9b9db
Paazlis <paazlismaswa@gmail.com>
2026-06-22 19:57:57 +0700

    Create Auto Click

--- Mods/Universal/AutoClick.lua
+++ Mods/Universal/AutoClick.lua
@@ -33,9 +33,6 @@ local Clicking=false
 local ClickSpeed=0.01
 local ClickThread=nil
 local ClickPoint=UserInputService:GetMouseLocation()
-local WaitType="Default"
-local SuperActive=false
-local MouseWaitDisabled=false
 
 local Window=UI:CreateWindow({Name="Maswa Clicker",Destroying=function()
 	WaitType="Default"
@@ -46,28 +43,16 @@ end})
 
 local Status=Window:AddLabel({Name="Point: "..tostring(ClickPoint)})
 
-local function ChooseWait(duration)
-	if SuperActive then
-		return RunService.RenderStepped:Wait()
-	else
-		if WaitType and WaitType=="Fast" then
-			if not duration then return RunService.RenderStepped:Wait() end
-			local start=tick()
-			while tick()-start<duration do RunService.RenderStepped:Wait() end
-			return start-duration
-		else
-			return task.wait(duration)
-		end
-	end
+local function FastWait(duration)
+	if not duration then return RunService.RenderStepped:Wait() end
+	local start=tick()
+	while tick()-start<duration do RunService.RenderStepped:Wait() end
+	return start-tick()
 end
 
-local function Mouse1Click(x,y)
+local function SendClick(x,y)
 	VirtualInputManager:SendMouseButtonEvent(x,y,0,true,game,0)
-	if MouseWaitDisabled then
-		ChooseWait()
-	else
-		task.wait()
-	end
+	FastWait()
 	VirtualInputManager:SendMouseButtonEvent(x,y,0,false,game,0)
 end
 
@@ -75,8 +60,8 @@ end
 ClickThread=task.spawn(function()
 	while true do
 		if Clicking then
-			Mouse1Click(ClickPoint.X,ClickPoint.Y)
-			ChooseWait(ClickSpeed)
+			SendClick(ClickPoint.X,ClickPoint.Y)
+			FastWait(ClickSpeed)
 		else
 			task.wait()
 		end
@@ -96,7 +81,9 @@ local ClickSpeedSlider=Window:AddSlider({
 	Range={0.001,100},
 	Value=ClickSpeed,
 	Callback=function(speed)
-		ClickSpeed=speed
+		if speed>0 then
+			ClickSpeed=speed
+		end
 	end
 })
 
@@ -110,25 +97,6 @@ Window:AddButton({
 	end
 })
 
-Window:AddToggle({
-	Name="Mouse Wait Disabled",
-	Value=false,
-	Callback=function(value)
-		MouseWaitDisabled=value
-	end
-})
-
-Window:AddSelector({
-	Name="Wait Type",
-	Options={"Default","Fast","Super"},
-	Value=WaitType,
-	Callback=function(value)
-		WaitType=value
-		SuperActive=value=="Super"
-		ClickSpeedSlider.Visible=value~="Super"
-	end
-})
-
-local Folder=Window:AddFolder("Creator",true)
+local Folder=Window:AddFolder("Creator")
 Folder:AddLabel("YouTube: Crokyreo")
 Folder:AddLabel("stav")

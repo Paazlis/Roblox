80fb2d0ac71d193fbf3f9b6f68372f3ec17ae8f6
Paazlis <paazlismaswa@gmail.com>
2026-06-25 21:30:28 +0700

    Update SecuretheAirport.lua v5

--- Mods/Games/SecuretheAirport.lua
+++ Mods/Games/SecuretheAirport.lua
@@ -12,35 +12,38 @@ local LocalPlayer = Players.LocalPlayer
 local npcs=Workspace.WorkspaceScriptable.Storage.NormalStorage.NPCWorkspace
 local realItem=ReplicatedStorage.Resources.NPCAssets.Items.RealContraband
 local ESPNPCEnabled,npcAdded=false,nil
---[[
+
 local function unespNpc(npc)
    local humanoid=npc:FindFirstChildOfClass("Humanoid")
    if humanoid and humanoid.DisplayDistanceType=="Viewer" then 
       humanoid.DisplayDistanceType="Subject"
    end
 end
-]]
+
 local Window = UI:CreateWindow({Name="Secure the Airport",Destroying=function()
     ESPNPCEnabled=false
     if npcAdded then npcAdded:Disconnect() npcAdded=nil end
-   -- for i,v in ipairs(npcs:GetChildren()) do unespNpc(v) end
+    for i,v in ipairs(npcs:GetChildren()) do unespNpc(v) end
 end})
 
 
 local function espNpc(npc)
     local xrayVisible=npc.XrayVisible
       
-    local bans={}
+    local denied=false
     for i,item in ipairs(xrayVisible:GetChildren()) do
        for j,object in ipairs(realItem:GetChildren()) do
            if string.find(item.Name,object.Name) then
-              table.insert(bans,item)
+              denied=true
               break
            end
         end
      end
-  
-     if #bans > 0 and ESPNPCEnabled then
+     local fakePassport=npc.Properties.RandomVariables.FakePassport
+     if fakePassport.Value then
+        denied=true
+     end
+     if denied and ESPNPCEnabled then
         local humanoid=npc:FindFirstChildOfClass("Humanoid")
         if humanoid then humanoid.DisplayDistanceType="Viewer" end
      end
@@ -50,20 +53,20 @@ end
 
 
 
-Window:AddToggle("ESP NPC",true,function(value)
+Window:AddToggle("ESP NPC",false,function(value)
     ESPNPCEnabled=value
     if npcAdded then npcAdded:Disconnect() npcAdded=nil end
     if value then
        npcAdded=npcs.ChildAdded:Connect(espNpc)
        for i,v in ipairs(npcs:GetChildren()) do espNpc(v) end
     else
-       --for i,v in ipairs(npcs:GetChildren()) do unespNpc(v) end
+       for i,v in ipairs(npcs:GetChildren()) do unespNpc(v) end
     end
 end)
 
---Window:AddToggle("ESP Luggage",true,function(value)
+Window:AddToggle("ESP Luggage",false,function(value)
 
---end)
+end)
 
 Window:AddLabel("YouTube: Crokyreo")
 

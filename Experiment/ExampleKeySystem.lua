4982dd597eadc61408bebab0776d4fe775853aed
Paazlis <paazlismaswa@gmail.com>
2026-06-27 12:28:48 +0700

    Create Example Key System

---
+++ Experiment/ExampleKeySystem.lua
@@ -0,0 +1,17 @@
+local UI=loadstring(game:HttpGet("http://raw.githubusercontent.com/Paazlis/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
+
+local KeySystem=UI:CreateKeySystem({
+	Title="Example Games",
+	Description="Key System",
+	UseNonce=true,
+	FileName="CrokyreoKey",
+	FolderName="Rayfield",
+	ServiceId=24751,
+	PlatoSecret="4ce8695c-5918-4704-a778-786e74424a0e",
+	Secret="1234",
+	ShowScript=true,
+	ScriptURL="https://raw.githubusercontent.com/Paazlis/Roblox/refs/heads/main/Packages/Sampluy/ExampleSampluy.lua",
+	ShowYoutube=true,
+	YoutubeURL="https://www.youtube.com/@Crokyreo?sub_confirmation=1",
+	Name="Drop Balls For Brainrots Key",
+})

43a6418a62d04d5e37e0a1e93ee200814dc15ec5
Paazlis <paazlismaswa@gmail.com>
2026-06-28 21:27:33 +0700

    Update ChickenFarm v4

--- Mods/Games/ChickenFarm.lua
+++ Mods/Games/ChickenFarm.lua
@@ -1,6 +1,10 @@
-local UI=loadstring(game:HttpGet("http://raw.githubusercontent.com/Paazlis/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
+local UI = loadstring(game:HttpGet("http://raw.githubusercontent.com/Paazlis/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
 
-local Players=game:GetService("Players")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local EggAdded = nil
+local DepositEnabled, UpgradeEnabled, MergeEnabled, CashEnabled = false, false, false, false
 
 local function FireTouch(hitPart, targetPart)
    if firetouchinterest then
@@ -10,34 +14,29 @@ local function FireTouch(hitPart, targetPart)
    end
 end
 
-local LocalPlayer=Players.LocalPlayer
-
-local EggAdded=nil
-local DepositEnabled,UpgradeEnabled,MergeEnabled,CashEnabled=false,false,false,false
-
-local Window=UI:CreateWindow({Name="Chicken Farm", Destroying=function()
-     if EggAdded then EggAdded:Disconnect() EggAdded=nil end
-     DepositEnabled,UpgradeEnabled,MergeEnabled,CashEnabled=false,false,false,false
+local Window = UI:CreateWindow({Name = "Chicken Farm", Destroying = function()
+     if EggAdded then EggAdded:Disconnect() EggAdded = nil end
+     DepositEnabled, UpgradeEnabled, MergeEnabled, CashEnabled = false, false, false, false
 end})
 
 Window:AddToggle({
 	Name = "Collect Egg",
     Value = false,
 	Callback = function(value)
-     if EggAdded then EggAdded:Disconnect() EggAdded=nil end
+     if EggAdded then EggAdded:Disconnect() EggAdded = nil end
      if value then
-        EggAdded=workspace.Eggs.ChildAdded:Connect(function(model)
+        EggAdded = workspace.Eggs.ChildAdded:Connect(function(model)
             task.wait(1)
             if model.Parent then
-               local targetPart=model:FindFirstChild("Part")
+               local targetPart = model:FindFirstChild("Part")
                if targetPart and LocalPlayer.Character then
                   FireTouch(LocalPlayer.Character.PrimaryPart, targetPart)
                end
             end
         end)
-        for i,model in ipairs(workspace.Eggs:GetChildren()) do
+        for i, model in ipairs(workspace.Eggs:GetChildren()) do
             if model.Parent then
-               local targetPart=model:FindFirstChild("Part")
+               local targetPart = model:FindFirstChild("Part")
                if targetPart and LocalPlayer.Character then
                   FireTouch(LocalPlayer.Character.PrimaryPart, targetPart)
                end
@@ -47,26 +46,25 @@ Window:AddToggle({
   end
 })
 
---[[
 local function getPlot()
-   for i,v in ipairs(workspace.Plots:GetChildren()) do
-      if v.Name==LocalPlayer.Name then return v end
+   for i, v in ipairs(workspace.Plots:GetChildren()) do
+      if v.Name == LocalPlayer.Name then return v end
    end
 end
 
-local Plot=getPlot()
+local Plot = getPlot()
 
 Window:AddToggle({
-	Text = "Auto Deposit",
+	Name = "Auto Deposit",
     Value = false,
-	 Callback = function(value)
-     DepositEnabled=false
+	Callback = function(value)
+     DepositEnabled = value
      if value then
         task.spawn(function()
             while DepositEnabled do
                task.wait(1)
                if Plot then
-                  local targetPart=Plot.Buttons.DepositEggs.Hitbox
+                  local targetPart = Plot.Buttons.DepositEggs.Hitbox
                   if targetPart and LocalPlayer.Character then
                      FireTouch(LocalPlayer.Character.PrimaryPart, targetPart)
                   end
@@ -78,16 +76,16 @@ Window:AddToggle({
 })
 
 Window:AddToggle({
-	Text = "Auto Merge",
-  Value = false,
+	Name = "Auto Merge",
+    Value = false,
 	Callback = function(value)
-     MergeEnabled=false
+     MergeEnabled = value
      if value then
         task.spawn(function()
             while MergeEnabled do
                task.wait(1)
 			   if Plot then
-                  local targetPart=Plot.Buttons.MergeChickens.Button
+                  local targetPart = Plot.Buttons.MergeChickens.Button
                   if targetPart and LocalPlayer.Character then
                      FireTouch(LocalPlayer.Character.PrimaryPart, targetPart)
                   end
@@ -99,16 +97,16 @@ Window:AddToggle({
 })
 
 Window:AddToggle({
-	Text = "Collect Cash",
-  Value = false,
+	Name = "Collect Cash",
+    Value = false,
 	Callback = function(value)
-     CashEnabled=false
+     CashEnabled = value
      if value then
         task.spawn(function()
             while CashEnabled do
                task.wait(1)
 			   if Plot then
-                  local targetPart=Plot.Buttons.CollectMoney.Button
+                  local targetPart = Plot.Buttons.CollectMoney.Button
                   if targetPart and LocalPlayer.Character then
                      FireTouch(LocalPlayer.Character.PrimaryPart, targetPart)
                   end
@@ -120,17 +118,17 @@ Window:AddToggle({
 })
 
 Window:AddToggle({
-	Text = "Upgrade All",
+	Name = "Upgrade All",
     Value = false,
 	Callback = function(value)
-     UpgradeEnabled=false
+     UpgradeEnabled = value
      if value then
         task.spawn(function()
             while UpgradeEnabled do
                task.wait(1)
 			   if Plot then
-                  for i,model in ipairs(Plot.Buttons.BuyChickens:GetChildren()) do
-                     local targetPart=model:FindFirstChild("Button")
+                  for i, model in ipairs(Plot.Buttons.BuyChickens:GetChildren()) do
+                     local targetPart = model:FindFirstChild("Button")
                      if targetPart and LocalPlayer.Character then
                         FireTouch(LocalPlayer.Character.PrimaryPart, targetPart)
                      end
@@ -141,6 +139,5 @@ Window:AddToggle({
      end
   end
 })
-]]
 
 Window:AddLabel("YouTube: Crokyreo")

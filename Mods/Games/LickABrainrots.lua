db2752a231272f688b06522a69ceae519a21cd0c
Paazlis <paazlismaswa@gmail.com>
2026-07-03 17:52:12 +0700

    Lick A Brainrots V2 | Working

--- Mods/Games/LickABrainrots.lua
+++ Mods/Games/LickABrainrots.lua
@@ -5,6 +5,7 @@ local Players = Services.Players
 local ReplicatedStorage = Services.ReplicatedStorage
 
 local LocalPlayer = Players.LocalPlayer
+local PlayerGui = LocalPlayer.PlayerGui
 
 local BuyEnabled, CashEnabled, TrainEnabled, RebirthEnabled, Farming = false, false, false, false, false
 
@@ -17,12 +18,9 @@ local function AutoTrain()
                 local gym = LocalPlayer.Backpack:FindFirstChild("Gym")
                 if gym then
                     pcall(function()
-                        if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid") then
-                            LocalPlayer.Character.Humanoid:EquipTool(gym)
-                        end
+                        LocalPlayer.Character.Humanoid:EquipTool(gym)
                     end)
                 end
-                
                	ReplicatedStorage.Remotes.doubleStrength:FireServer()
             end
         end)
@@ -36,45 +34,10 @@ local function AutoFarming()
             while Farming do
                 task.wait(0.5)
                 task.spawn(function()
-                    pcall(function()
-                        ReplicatedStorage.Remotes.OnCast:InvokeServer(1)
-                        ReplicatedStorage.Remotes.StartRun:InvokeServer()
-                        ReplicatedStorage.Remotes.FinishRun:InvokeServer(true)
-                    end)
+                    ReplicatedStorage.Remotes.OnCast:InvokeServer(1)
+                    ReplicatedStorage.Remotes.StartRun:InvokeServer()
+                    ReplicatedStorage.Remotes.FinishRun:InvokeServer(true)
                 end)
-            end
-        end)
-    end
-end
-
--- Collect Cash Function --
-local function AutoCash()
-    if CashEnabled then
-        task.spawn(function()
-            while CashEnabled do
-                task.wait(1)
-            end
-        end)
-    end
-end
-
--- Buy Building Function --
-local function AutoBuy()
-    if BuyEnabled then
-        task.spawn(function()
-            while BuyEnabled do
-                task.wait(1)
-            end
-        end)
-    end
-end
-
--- Rebirth Function --
-local function AutoRebirth()
-    if RebirthEnabled then
-        task.spawn(function()
-            while RebirthEnabled do
-                 task.wait(5)
             end
         end)
     end
@@ -82,7 +45,7 @@ end
 
 -- Main UI --
 local Window = UI:CreateWindow({
-    Name = "Paper Plane For Brainrots", 
+    Name = "Lick A Brainrots", 
     Destroying = function()
         BuyEnabled, CashEnabled, TrainEnabled, RebirthEnabled, Farming = false, false, false, false, false
     end
@@ -106,32 +69,5 @@ Window:AddToggle({
     end
 })
 
-Window:AddToggle({
-    Text = "Collect Cash",
-    Value = false,
-    Callback = function(value)
-        CashEnabled = value
-        AutoCash()
-    end
-})
-
-Window:AddToggle({
-    Text = "Buy Building",
-    Value = false,
-    Callback = function(value)
-        BuyEnabled = value
-        AutoBuy()
-    end
-})
-
-Window:AddToggle({
-    Text = "Auto Rebirth",
-    Value = false,
-    Callback = function(value)
-        RebirthEnabled = value
-        AutoRebirth()
-    end
-})
-
-Window:AddLabel({Text = "YouTube: Crokyreo"})
-Window:AddLabel({Text = "YouTube: vaehz"})
+Window:AddLabel("YouTube: Crokyreo")
+Window:AddLabel("YouTube: vaehz")

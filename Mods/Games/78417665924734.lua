e654679ef1903c744f8d625fdeb44531daa0b4cd
Paazlis <paazlismaswa@gmail.com>
2026-07-04 17:01:45 +0700

    Build a Slime Defense V2 | Working

--- Mods/Games/78417665924734.lua
+++ Mods/Games/78417665924734.lua
@@ -1,19 +1,18 @@
 local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Paazlis/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
 local Utility = loadstring(game:HttpGet("https://raw.githubusercontent.com/Paazlis/Roblox/refs/heads/main/Packages/Utility/init.luau"))()
+local Instancer = loadstring(game:HttpGet("http://raw.githubusercontent.com/Paazlis/Roblox/refs/heads/main/Packages/Instancer/init.luau"))()
 
 local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
 local Players = Services.Players
 local ReplicatedStorage = Services.ReplicatedStorage
-local VirtualInputManager = Services.VirtualInputManager
-local UserInputService = Services.UserInputService
 
 local LocalPlayer=Players.LocalPlayer
 local PlayerGui=LocalPlayer.PlayerGui
 
 local UpgradeEnabled, AttackEnabled = false, false
 local NpcAddedConnection, NpcRemovedConnection = nil, nil
-local ClickPoint=UserInputService:GetMouseLocation()
 local NpcActives = {}
+local NpcTarget = nil
 
 local function GetPlot()
 	local plots = workspace:FindFirstChild("Map") and workspace.Map:FindFirstChild("Plots")
@@ -22,7 +21,7 @@ local function GetPlot()
 	for _, base in pairs(plots:GetChildren()) do
 		local owner = base:GetAttribute("Owner")
 		if owner and LocalPlayer.Name then
-			 return base
+			return base
 		end
 	end
 
@@ -31,85 +30,127 @@ end
 
 local Plot = GetPlot()
 
--- Train Function --
+-- Upgrade Function --
 local function AutoUpgrade()
 	if UpgradeEnabled then
 		task.spawn(function()
 			while UpgradeEnabled do
-				 task.wait(1)
-				 Plot = Plot or GetPlot()
-         if Plot then
-            local structures = Plot.Main.Plot.Structures
-            for _, structure in ipairs(structures:GetChildren()) do
-                ReplicatedStorage.Remotes.Plot.Building:InvokeServer("UpgradeStructure", structure.Name)
-            end
-            task.wait(1)
-            ReplicatedStorage.Remotes.Plot.UpgradeKing:InvokeServer()
-         end
+				task.wait(5)
+				Plot = Plot or GetPlot()
+				if Plot then
+					local structures = Plot.Main.Plot.Structures
+					for _, structure in ipairs(structures:GetChildren()) do
+						task.wait()
+						if UpgradeEnabled then
+							ReplicatedStorage.Remotes.Plot.Building:InvokeServer("UpgradeStructure", structure.Name)
+						end
+					end
+				end
+				task.wait(5)
+				if UpgradeEnabled then
+					ReplicatedStorage.Remotes.Plot.UpgradeKing:InvokeServer()
+				end
 			end
 		end)
 	end
 end
 
 -- Attack Function --
-local function AutoAttack()
-   NpcAddedConnection = Utility.Cleanup(NpcAddedConnection)
-   NpcRemovedConnection = Utility.Cleanup(NpcRemovedConnection)
-  
-	 if AttackEnabled then
-      Plot = Plot or GetPlot()
-      if not Plot then break end
-    
-      local npcs = Plot.Npcs
-    
-      NpcAddedConnection = npcs.ChildAdded:Connect(function(v)
-         NpcActives[v] = v
-      end
+local function FindClosestNpc()
+	local target = nil
+	local shortestDistance = math.huge
+	local maxDistance = 500
 
-      NpcRemovedConnection = npcs.RwmovedAdded:Connect(function(v)
-         NpcActives[v] = nil
-      end
-        
-      task.spawn(function() 
-          while AttackEnabled do
-             task wait(0.5)
-             
-             for k, v in pairs(NpcActives) do
-                 LocalPlayer.Character:MoveTo(v.PrimaryPart)
-             end
-          end
-      end)
-      
-      task.spawn(function()
-          local sword = nil
-          while AttackEnabled do
-             task.wait(1)
-             local character = LocalPlayer.Character
-             if character then
-                if not sword or sword.Parent ~= character then
-                   for _, tool in ipairs(character:GetChildren()) do
-                       if tool:IsA("Tool") and tool:FindFirstChild("Handle") then
-                          sword = tool
-                          break
-                       end
-                    end
-                end
-                if sword and sword.Parent == character then
-                   firesignal(sword.Activated)
-                end
-             end
-          end
-      end)
-	 end
+	local character = LocalPlayer.Character
+
+	if not character or not character.PrimaryPart then
+		return nil
+	end
+
+	local playerPos = character.PrimaryPart.Position
+
+	for _, npc in ipairs(NpcActives) do
+		if npc and npc.PrimaryPart and not Instancer.IsDied(npc) then
+			local dist = (npc.PrimaryPart.Position - playerPos).Magnitude
+
+			if dist < shortestDistance and dist <= maxDistance then
+				target = npc
+				shortestDistance = dist
+			end
+		end
+	end
+
+	return target
+end
+
+local function AutoAttack()
+	NpcAddedConnection = Utility.Cleanup(NpcAddedConnection)
+	NpcRemovedConnection = Utility.Cleanup(NpcRemovedConnection)
+
+	if AttackEnabled then
+		Plot = Plot or GetPlot()
+		if not Plot then break end
+
+		local npcs = Plot.Npcs
+
+		NpcAddedConnection = npcs.ChildAdded:Connect(function(model)
+			local humanoid = model:FindFirstChildOfClass("Humanoid")
+			if humanoid and humanoid.Health > 0 then
+				NpcActives[model] = model
+			end
+		end)
+
+		NpcRemovedConnection = npcs.ChildRemoved:Connect(function(model)
+			NpcActives[model] = nil
+		end)
+
+		task.spawn(function() 
+			while AttackEnabled do
+				task.wait()
+				
+				if Instancer.IsDied(NpcTarget) then
+					NpcTarget = FindClosestNpc()
+				end
+				
+				local character = LocalPlayer.Character
+				if not Instancer.IsDied(character) and NpcTarget.PrimaryPart and AttackEnabled then
+					character:MoveTo(NpcTarget.PrimaryPart.Position)
+				end
+			end
+		end)
+
+		task.spawn(function()
+			local sword = nil
+			while AttackEnabled do
+				task.wait(1)
+				local character = LocalPlayer.Character
+				if character then
+					if not sword or sword.Parent ~= character then
+						for _, tool in ipairs(character:GetChildren()) do
+							if tool:IsA("Tool") and tool:FindFirstChild("Handle") then
+								sword = tool
+								break
+							end
+						end
+					end
+					if sword and sword.Parent == character then
+						if firesignal then
+							firesignal(sword.Activated)
+						end
+					end
+				end
+			end
+		end)
+	end
 end
 
 -- Main UI --
 local Window = UI:CreateWindow({
 	Name = "Build a Slime Defense", 
 	Destroying = function()
-		 UpgradeEnabled, AttackEnabled = false, false
-		 NpcAddedConnection = Utility.Cleanup(NpcAddedConnection)
-     NpcRemovedConnection = Utility.Cleanup(NpcRemovedConnection)
+		UpgradeEnabled, AttackEnabled = false, false
+		NpcAddedConnection = Utility.Cleanup(NpcAddedConnection)
+		NpcRemovedConnection = Utility.Cleanup(NpcRemovedConnection)
 	end
 })
 

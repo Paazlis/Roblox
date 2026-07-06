d663d9780ac2432ccfec80b84ee56a3f80f4f1d0
Paazlis <paazlismaswa@gmail.com>
2026-07-07 04:06:04 +0700

    Drain the Lake V6 | Working

--- Mods/Games/138381251771774.lua
+++ Mods/Games/138381251771774.lua
@@ -7,9 +7,6 @@ local ReplicatedStorage = Services.ReplicatedStorage
 local LocalPlayer = Players.LocalPlayer
 local PlayerGui = LocalPlayer.PlayerGui
 local Farming = false
-local DrainConnection = nil
-local Connections = {}
-local DrainActives = {}
 
 local function IsFillFull()
 	if PlayerGui.Interface.Holder.BucketFill.Bar.Scale.Size.X.Scale >= 1 then
@@ -18,59 +15,15 @@ local function IsFillFull()
 	return false
 end
 
-local function CheckPointAdded(checkpoint)
-	if checkpoint and checkpoint.Parent and tonumber(checkpoint.Name) then
-		for _, model in ipairs(checkpoint:GetChildren()) do
-			if model.Name:lower():find("drain") and not table.find(DrainActives, model) and model:FindFirstChild("Scripted") then
-				table.insert(DrainActives, model)
-				local ancestryChangedConnection
-				ancestryChangedConnection = model.AncestryChanged:Connect(function(_, parent)
-					if not parent then
-						ancestryChangedConnection:Disconnect()
-
-						local index1 = table.find(DrainActives, model)
-						if index1 then
-							table.remove(DrainActives, index1)
-						end
-
-						local index2 = table.find(Connections, ancestryChangedConnection)
-						if index2 then
-							table.remove(Connections, index2)
-						end
-					end
-				end)
-				table.insert(Connections, ancestryChangedConnection)
-			end
-		end
-	end
-end
-
-local function ApplyDrains()
-	local checkpoints = workspace.Scripted.CheckpointParts
-
-	DrainConnection = checkpoints.ChildAdded:Connect(CheckPointAdded)
-
-	for _, checkpoint in ipairs(checkpoints:GetChildren()) do
-		CheckPointAdded(checkpoint)
-	end
-end
-
 local Window = UI:CreateWindow({
 	Name = "Drain the Lake",
 	Destroying = function()
 		Farming = false
-		if DrainConnection then DrainConnection:Disconnect() DrainConnection = nil end
-		for _, connection in ipairs(Connections) do
-			if connection then
-				connection:Disconnect()   
-			end
-		end
-		table.clear(Connections)
 	end
 })
 
 Window:AddToggle({
-	Text = "Auto Farming", 
+	Text = "Auto Drain", 
 	Value = false, 
 	Callback = function(value)
 		Farming = value
@@ -79,41 +32,25 @@ Window:AddToggle({
 				while Farming do
 					task.wait()
  							
-					if not IsFillFull() then
+					if not IsFillFull() and Farming then
 						ReplicatedStorage.VerdantRemotes["VDT_Bucket.Used"]:FireServer()
 					end
-							--[[
-					for i = #DrainActives, 1, -1 do
-						local drain = DrainActives[i]
-						if drain and drain.Parent then
-							task.wait()
-									
-							local scripted = drain.Scripted
-							local drainPrompt = scripted.ProximityPosition.ProximityPrompt
-							local tokensPrompt = scripted.TakeTokens.ProximityPrompt
-
-							if tokensPrompt and drainPrompt and tokensPrompt.Enabled and not drainPrompt.Enabled and Farming then
-                               task.wait(2)
-							   if tokensPrompt.Enabled and not drainPrompt.Enabled then
-							      ReplicatedStorage.VerdantRemotes["VDT_Tokens.Take"]:FireServer(drainPrompt)
-							   end
-							end
-									
-							if drainPrompt and drainPrompt.Enabled and IsFillFull() and Farming then
-								ReplicatedStorage.VerdantRemotes["VDT_Bucket.Poured"]:FireServer(drainPrompt)
-							end
-						end
-					end
-							]]
 				end
 			end)
 		end
 	end
 })
 
-Window:AddLabel("YouTube: Crokyreo")
+Window:AddButton({
+	Text = "Claim All Chest",
+    Callback = function()
+		for _, chest in ipairs(workspace.Scripted.Chests:GetChildren()) do
+			local part = chest:FindFirstChild("Part")
+		    if part then
+		       ReplicatedStorage.VerdantRemotes["VDT_Chest.Open"]:FireServer(part)
+			end
+		end
+	end
+})
 
-pcall(function()
-	ApplyDrains()
-	return nil
-end)
+Window:AddLabel("YouTube: Crokyreo")

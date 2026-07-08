3d63c1824280d3952f4a8fd64da176ed2e45b0e1
Paazlis <paazlismaswa@gmail.com>
2026-07-09 00:14:01 +0700

    Update 99702578544768.lua V5

--- Mods/Games/99702578544768.lua
+++ Mods/Games/99702578544768.lua
@@ -9,26 +9,65 @@ local PlayerGui = LocalPlayer.PlayerGui
 
 local CashEnabled = false
 local CashIndex = 0
+local RebirthConnection = nil
+local TrainTapConnection = nil
+
+local function FindByPath(instance, array)
+    local function Search(current, index)
+		if not current then return nil end
+		
+        if index > #array then
+            return current
+        end
+
+        local targetName = array[index]
+
+        for _, child in ipairs(current:GetChildren()) do
+            if child.Name == targetName then
+                local result = Search(child, index + 1)
+                
+                if result then
+                    return result
+                end
+			end
+        end
+        
+        return nil
+    end
+
+    return Search(instance, 1)
+end
 
 -- Auto Rebirth --
-game:GetService("Players").LocalPlayer.PlayerGui["1"]["1"]["33"]["33"]["2"]["4"]["3"]["3"]["1"]
-
-game:GetService("Players").LocalPlayer.PlayerGui["1"]["1"]["33"]["33"]["2"]["4"]["3"]["4"]["2"]["2"]
+--game:GetService("Players").LocalPlayer.PlayerGui["1"]["1"]["33"]["33"]["2"]["4"]["3"]["3"]["1"]
+--game:GetService("Players").LocalPlayer.PlayerGui["1"]["1"]["33"]["33"]["2"]["4"]["3"]["4"]["2"]["2"]
 
 -- Auto Tap --
 -- game:GetService("Players").LocalPlayer.PlayerGui["1"]["1"]:GetChildren()[8]["1"]
-
-game:GetService("Players").LocalPlayer.PlayerGui["1"]["1"]:GetChildren()[8]["1"]["1"]["3"]
+-- game:GetService("Players").LocalPlayer.PlayerGui["1"]["1"]:GetChildren()[8]["1"]["1"]["3"]
 
 local Window = UI:CreateWindow({
 	Name = "BE A FISH BAIT",
 	Destroying = function()
-		 CashEnabled = false
+	    CashEnabled = false
+		if RebirthConnection then RebirthConnection:Disconnect() RebirthConnection = nil end
+	    if TrainTapConnection then TrainTapConnection:Disconnect() TrainTapConnection = nil end
 	end
 })
 
 Window:AddToggle({
-	Text = "Collect Cash [X]", 
+	Text = "Auto Tap", 
+	Value = false, 
+	Callback = function(value)
+		if TrainTapConnection then TrainTapConnection:Disconnect() TrainTapConnection = nil end
+		if value then
+			
+		end
+	end
+})
+
+Window:AddToggle({
+	Text = "Collect Cash", 
 	Value = false, 
 	Callback = function(value)
 		CashEnabled = value
@@ -48,10 +87,23 @@ Window:AddToggle({
 	Text = "Auto Rebirth", 
 	Value = false, 
 	Callback = function(value)
-		RebirthEnabled = value
-		
+		if RebirthConnection then RebirthConnection:Disconnect() RebirthConnection = nil end
+		if value then
+		   local RebirthFrame = FindByPath(PlayerGui, {"1", "1", "33", "33", "2", "4", "3"})
+           local RebirthFill = FindByPath(RebirthFrame, {"3", "1"})
+           local RebirthButton = FindByPath(RebirthFrame, {"4", "2","2"})
+				
+           RebirthConnection = RebirthFill:GetPropertyChangedSignal("Size"):Connect(function()
+               if RebirthFill.Size.X.Scale >= 1 then
+                  firesignal(RebirthButton.MouseButton1Click)
+			   end
+		   end)
+
+		   if RebirthFill.Size.X.Scale >= 1 then
+              firesignal(RebirthButton.MouseButton1Click)
+		   end
+		end
 	end
 })
-
 
 Window:AddLabel("YouTube: Crokyreo")

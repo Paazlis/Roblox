4a8e8039690a3f2e71592debd14efa3963bdb584
Paazlis <paazlismaswa@gmail.com>
2026-07-10 07:13:15 +0700

    Vacuum Simulator V2 Working

--- Mods/Games/77133321531129.lua
+++ Mods/Games/77133321531129.lua
@@ -1,4 +1,4 @@
-local UI = loadstring(game:HttpGet("http://raw.githubusercontent.com/Paazlis/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
 
 local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
 local Players = Services.Players

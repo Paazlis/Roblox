88a79231c952ced5ac7f845d77a83c28f994d95f
Paazlis <paazlismaswa@gmail.com>
2026-07-10 07:59:38 +0700

    +1 Shrink Per Step V1 Working

--- Mods/Games/91695551099066.lua
+++ Mods/Games/91695551099066.lua
@@ -1,2 +1,54 @@
-game:GetService("Players").LocalPlayer.PlayerGui.Main.Frames.Rebirths.Progress.CanvasGroup.Bar
-game:GetService("Players").LocalPlayer.PlayerGui.Main.Frames.Rebirths.RebirthButton
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
+
+local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
+local Players = Services.Players
+local ReplicatedStorage = Services.ReplicatedStorage
+
+local LocalPlayer = Players.LocalPlayer
+local PlayerGui = LocalPlayer.PlayerGui
+
+local RebirthConnection = nil
+
+local function FireButton(button)
+	if firesignal then
+		firesignal(button.Activated)
+		firesignal(button.MouseButton1Click)
+	end
+end
+
+local function IsFillFull(fill)
+	if fill.Size.X.Scale >= 1 and fill.Size.X.Scale <= 0.98 then
+		return true
+	end
+	return false
+end
+
+local Window = UI:CreateWindow({
+	Name = "+1 Shrink Per Step",
+	Destroying = function()
+		if RebirthConnection then RebirthConnection:Disconnect() RebirthConnection = nil end
+	end
+})
+
+Window:AddToggle({
+	Text = "Auto Rebirth",
+	Value = false,
+	Flag = "rebirth_enabled",
+	Callback = function(value)
+		if RebirthConnection then RebirthConnection:Disconnect() RebirthConnection = nil end
+		if value then
+			local rebirthFill = PlayerGui.Main.Frames.Rebirths.Progress.CanvasGroup.Bar
+			local rebirthButton = PlayerGui.Main.Frames.Rebirths.RebirthButton
+			RebirthConnection = rebirthFill:GetPropertyChangedSignal("Size"):Connect(function()
+				if IsFillFull(rebirthFill) then
+					FireButton(rebirthButton)
+				end
+			end)
+			if IsFillFull(rebirthFill) then
+				FireButton(rebirthButton)
+			end
+		end
+	end
+})
+
+Window:AddLabel("YouTube: Crokyreo")

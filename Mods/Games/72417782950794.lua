428b003ea06c4e679f39b9e895f2b0a85ea8ae50
Paazlis <paazlismaswa@gmail.com>
2026-07-11 09:27:30 +0700

    Clean the Backyard V43 Working Default

--- Mods/Games/72417782950794.lua
+++ Mods/Games/72417782950794.lua
@@ -1,4 +1,5 @@
 local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
+local Instancer = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Instancer/init.luau"))()
 
 local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
 local Players = Services.Players
@@ -13,7 +14,7 @@ local CleanEnabled = false
 local GrindingMachinePosition, VendingMachinePosition = Vector3.new(122, 18, 135), Vector3.new(122, 18, 158)
 
 local TrashFillConnection, EnergyFillConnection, DebrisAddedConnection, DebrisRemovedConnection, GameCleanConnection = nil, nil, nil, nil, nil
-local TrashDebounce, EnergyDebounce = false, false
+local FullGarbagebags, RunOutEnergy = false, false
 local FoodList = {"SodaCan", "EnergyBar"}
 local ItemsCache = {}
 local ItemsConnection = {}
@@ -42,16 +43,21 @@ local function IsCursorPerfect(cursor)
 	return false
 end
 
+local Character = LocalPlayer.Character
+
+local CharacterAddedConnection = LocalPlayer.CharacterAdded:Connect(function(newCharacter)
+	Character = newCharacter
+end)
+
 local Window = UI:CreateWindow({
 	Name = "Clean the Backyard",
 	Destroying = function()
 		CleanEnabled = false
-		TrashDebounce, EnergyDebounce = false, false
+		FullGarbagebags, RunOutEnergy = false, false
 		if GameCleanConnection then GameCleanConnection:Disconnect() GameCleanConnection = nil end
 		if TrashFillConnection then TrashFillConnection:Disconnect() TrashFillConnection = nil end
 		if EnergyFillConnection then EnergyFillConnection:Disconnect() EnergyFillConnection = nil end
-		if DebrisRemovedConnection then DebrisRemovedConnection:Disconnect() DebrisRemovedConnection = nil end
-		if DebrisAddedConnection then DebrisAddedConnection:Disconnect() DebrisAddedConnection = nil end
+		if CharacterAddedConnection then CharacterAddedConnection:Disconnect() CharacterAddedConnection = nil end
 		table.clear(ItemsCache)
 		for _, connection in ipairs(ItemsConnection) do if connection then connection:Disconnect() end end
 		table.clear(ItemsConnection)
@@ -68,73 +74,73 @@ Window:AddToggle({
 		-- Bersihkan koneksi lama agar tidak menumpuk (Memory Leak)
 		if TrashFillConnection then TrashFillConnection:Disconnect() TrashFillConnection = nil end
 		if EnergyFillConnection then EnergyFillConnection:Disconnect() EnergyFillConnection = nil end
-		if DebrisRemovedConnection then DebrisRemovedConnection:Disconnect() DebrisRemovedConnection = nil end
-		if DebrisAddedConnection then DebrisAddedConnection:Disconnect() DebrisAddedConnection = nil end
+		
 		table.clear(ItemsCache)
 		for _, connection in ipairs(ItemsConnection) do if connection then connection:Disconnect() end end
 		table.clear(ItemsConnection)
 
 		if value then
 			local energyFill = PlayerGui.InterfaceUI.StatsUI.Energy.ProgressBar.BarFrame
-			local trashFill = PlayerGui.InterfaceUI.StatsUI["Garbage Bag"].ProgressBar.BarFrame
+			local garbagebagsFill = PlayerGui.InterfaceUI.StatsUI["Garbage Bag"].ProgressBar.BarFrame
 			local itemSpawns = workspace:FindFirstChild("ItemSpawns")
-
-			TrashDebounce, EnergyDebounce = false, false
+			local spawnedDebris = workspace:FindFirstChild("SpawnedDebris")
+			
+			FullGarbagebags, RunOutEnergy = false, false
 
 			-- Deteksi otomatis jika tas penuh atau energi habis via UI
-			TrashFillConnection = trashFill:GetPropertyChangedSignal("Size"):Connect(function()
-				TrashDebounce = trashFill.Size.Y.Scale >= 0.98
+			TrashFillConnection = garbagebagsFill:GetPropertyChangedSignal("Size"):Connect(function()
+				FullGarbagebags = garbagebagsFill.Size.Y.Scale >= 0.98
 			end)
 
 			EnergyFillConnection = energyFill:GetPropertyChangedSignal("Size"):Connect(function()
-				EnergyDebounce = energyFill.Size.Y.Scale <= 0.2
+				RunOutEnergy = energyFill.Size.Y.Scale <= 0.2
 			end)
 
-
-			local spawnedDebris = workspace:FindFirstChild("SpawnedDebris")
-	
 			-- Loop Utama Auto Clean
 			task.spawn(function()
 				while CleanEnabled do
-					local character = LocalPlayer.Character
-					local primaryPart = character and character:FindFirstChild("HumanoidRootPart")
-
-					if not primaryPart then 
+					if not (Character and Character.Parent) then
+						LocalPlayer.CharacterAdded:Wait()
+					end
+					
+					local rootPart = Character:FindFirstChild("HumanoidRootPart") or Character.PrimaryPart
+					if not rootPart then 
 						FastWait() 
 						continue 
 					end
 
-					-- 1. LOGIK CEK ENERGI (Jika Habis)
-					if EnergyDebounce or energyFill.Size.Y.Scale <= 0.2 then
+					-- 1. Periksa Energi (Jika Habis)
+					if RunOutEnergy or energyFill.Size.Y.Scale <= 0.2 then
 						-- Teleport ke Vending Machine
-						character:PivotTo(CFrame.new(VendingMachinePosition + Vector3.new(0, 3, 0)))
+						Character:PivotTo(CFrame.new(Vector3.new(VendingMachinePosition.X, rootPart.Position.Y + 3, VendingMachinePosition.Z)))
 						FastWait(0.3)
 
 						-- Membeli minuman/makanan lewat Remote Event yang ada di catatan kaki Cobalt kamu
 						ReplicatedStorage.EVENTS.PlayerEvents.BuyRechargeItem:FireServer()
 						FastWait(1)
-
-						repeat FastWait(0.1) until spawnedDebris:FindFirstChild("SodaCan") or spawnedDebris:FindFirstChild("EnergyBar")
-
-						pcall(function()
-							local item = spawnedDebris:FindFirstChild("SodaCan") or spawnedDebris:FindFirstChild("EnergyBar")
-
+						
+						local energyItem = Instancer.YieldForChild(spawnedDebris, function(child)
+							return not RunOutEnergy or energyFill.Size.Y.Scale >= 0.2 or child.Name == "SodaCan" or child.Name == "EnergyBar"
+						end)
+						
+						if energyItem and energyItem.Name == "SodaCan" or energyItem.Name == "EnergyBar"  then
 							-- Ambil energi via Remote
-							ReplicatedStorage.EVENTS.PlayerEvents.CollectItem:FireServer(item)
-						    FastWait(1)
-										
-							ReplicatedStorage.EVENTS.PlayerEvents.ConsumeItem:FireServer(false, item.Name)
-                           
+							ReplicatedStorage.EVENTS.PlayerEvents.CollectItem:FireServer(energyItem)
+							FastWait(1)
+
+							ReplicatedStorage.EVENTS.PlayerEvents.ConsumeItem:FireServer(false, energyItem.Name)
+
 							task.wait(1)
 							ReplicatedStorage.EVENTS.PlayerEvents.ConsumeItem:FireServer(true)
-						end)
-						EnergyDebounce = false
-					end
 							
-					-- 2. LOGIK CEK TAS SAMPAH (Jika Penuh)
-					if TrashDebounce or trashFill.Size.Y.Scale >= 0.98 then
+							RunOutEnergy = energyFill.Size.Y.Scale >= 0.2
+						end
+					end
+
+					-- 2. Periksa Kantong Sampah (Jika Penuh)
+					if FullGarbagebags or garbagebagsFill.Size.Y.Scale >= 0.98 then
 						-- Teleport ke Grinding Machine / Tempat Pembuangan
-						character:PivotTo(CFrame.new(GrindingMachinePosition + Vector3.new(0, 3, 0)))
+						Character:PivotTo(CFrame.new(Vector3.new(GrindingMachinePosition.X, rootPart.Position.Y + 3, GrindingMachinePosition.Z)))
 						FastWait(0.4)
 
 						-- Membuang sampah
@@ -142,11 +148,11 @@ Window:AddToggle({
 							Vector3.new(0.96049702167511, -0.25137504935265, -0.11939886957407),
 							10
 						)
-						FastWait(0.5)
-						TrashDebounce = false
+						
+						FullGarbagebags = garbagebagsFill.Size.Y.Scale < 0.98
 					end
 
-					-- 3. PROSES ARCHIVE & LEOP AMBIL SAMPAH
+					-- 3. Periksa dan Proses Ambil Sampah
 					local itemFound = false
 					if itemSpawns then
 						for _, area in ipairs(itemSpawns:GetChildren()) do
@@ -159,20 +165,18 @@ Window:AddToggle({
 								if itemsFolder then
 									for _, item in ipairs(itemsFolder:GetChildren()) do
 										if not CleanEnabled then break end
-                                        if not item or not item.Parent then continue end
-												
+										if not item or not item.Parent then continue end
+
 										-- Interupsi jika tiba-tiba tas penuh atau energi habis saat sedang nge-loop item
-										if trashFill.Size.Y.Scale >= 0.98 or energyFill.Size.Y.Scale <= 0.2 then
+										if (FullGarbagebags or garbagebagsFill.Size.Y.Scale >= 0.98) or (RunOutEnergy or energyFill.Size.Y.Scale <= 0.2) then
 											break
 										end
-										
-										
-												
+
 										local part = item:FindFirstChildWhichIsA("BasePart")
 										if part then
 											itemFound = true
 											-- Teleport ke lokasi sampah
-											character:PivotTo(part.CFrame + Vector3.new(0, 3, 0))
+											Character:PivotTo(part.CFrame + Vector3.new(0, 3, 0))
 											FastWait(0.1)
 
 											-- Ambil sampah via Remote
@@ -194,5 +198,31 @@ Window:AddToggle({
 		end
 	end
 })
-									
+
+Window:AddButton({
+	Text = "Go Home",
+	MethodType = "DoubleClick",
+	MaxClick = 2,
+	ClickThreshold = 0.5,
+	ClickDuration = 2,
+	Callback = function()
+		-- 1. Ambil posisi dan rotasi (Pivot) karakter saat ini
+		local charPivot = Character:GetPivot()
+
+		-- 2. Ambil X dan Z dari part, dan Y dari karakter
+		local targetX = VendingMachinePosition.X
+		local targetZ = VendingMachinePosition.Z
+		local targetY = charPivot.Position.Y
+
+		-- 3. Buat posisi baru
+		local newPosition = Vector3.new(targetX, targetY, targetZ)
+
+		-- 4. Gabungkan rotasi asli karakter dengan posisi yang baru
+		local newCFrame = charPivot.Rotation + newPosition
+
+		-- 5. Pindahkan karakter
+		Character:PivotTo(newCFrame)
+	end
+})
+
 Window:AddLabel("YouTube: Crokyreo")

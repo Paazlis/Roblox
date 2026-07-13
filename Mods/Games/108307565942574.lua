45afb27e8f3c76dac3e266fe5e9f3fe1a20b700b
Paazlis <paazlismaswa@gmail.com>
2026-07-13 09:06:43 +0700

    RNG Heroes V1 Working Default

---
+++ Mods/Games/108307565942574.lua
@@ -0,0 +1,20 @@
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Center.Prestige.LevelBar.ProgressBar.UIGradient.Offset.X >= 0
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Center.Prestige.Prestige
+
+
+
+-- Auto Upgrade --
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Upgrades.Canvas.Content
+
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Upgrades.Canvas.Content["Tile_3 Enemies"]
+-- Attributes: UpgradeState = Owned, Affordable, Locked, OpenTab, Unaffordable
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Upgrades.Back
+
+-- auto collect --
+workspace.Loot
+ClassName = Model
+workspace.Loot:GetChildren()[11].BillboardGui
+BasePart to find

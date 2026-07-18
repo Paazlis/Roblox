29da4f40d667529d4f9d41eaa9691d411630ad25
Paazlis <paazlismaswa@gmail.com>
2026-07-19 03:18:43 +0700

    template v3

--- Mods/Games/template.lua
+++ Mods/Games/template.lua
@@ -10,20 +10,7 @@ local LocalPlayer = Players.LocalPlayer
 local PlayerGui = LocalPlayer.PlayerGui
 local Backpack = LocalPlayer:FindFirstChildOfClass("Backpack")
 
-local function FireTouch(hit, target)
-    if firetouchinterset then
-        firetouchinterset(hit, target, true)
-        task.wait()
-        firetouchinterset(hit, target, false)
-    end
-end
-
-local function FireButton(button)
-    if firesignal then
-        firesignal(button.Activated)
-        firesignal(button.MouseButton1Click)
-    end
-end
+local YieldForChild, FindByPath, FireButton, FireTouch = Instancer.YieldForChild, Instancer.FindByPath, Executier.FireButton, Executier.FireTouch
 
 local Window = UI:CreateWindow({
 	  Name = "Your Title",
@@ -34,8 +21,7 @@ local Window = UI:CreateWindow({
 
 local Toggle = Window:AddToggle({
 	  Text = "Toggle",
-    Value = false,
-	  Flag = "toggle",
+      Value = false,
 	  Callback = function(value)
       	print(value)
   	end
@@ -43,12 +29,21 @@ local Toggle = Window:AddToggle({
 
 local Slider = Window:AddSlider({
 	  Text = "Fov",
-    Value = 10,
+      Value = 10,
 	  Range = {70,170},
 	  Increment = 0.1,
-    Flag = "slider",
 	  Callback = function(value)
       	print("Fov:",value)
+    end
+})
+
+local Dropdown = Window:AddDropdown({
+	  Text = "Fruit Type",
+      Options = {"Apple", "Banana", "Avocado", "Durian"},
+	  Option = nil,
+	  MultipleOptions = true,
+	  Callback = function(option)
+      	print("Type:",table.concat(option,","))
     end
 })
 

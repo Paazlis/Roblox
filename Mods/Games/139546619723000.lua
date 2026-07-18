4647643cd37e84bcc08ed678f93d6a2a8f0658de
Paazlis <paazlismaswa@gmail.com>
2026-07-18 14:02:16 +0700

    My Giant Sandwich V1

---
+++ Mods/Games/139546619723000.lua
@@ -0,0 +1,17 @@
+Plot is OwnerUserId
+
+-- collect cash --
+workspace.Plots.Plot_1.CollectButtons.CollectButton1.Hitbox
+
+-- auto roll --
+workspace.Plots.Plot_1.CrateSpawn.CrateFolder
+workspace.Plots.Plot_1.SpawnLever.Base.ProximityPrompt
+
+Frame is rarity
+game:GetService("Players").LocalPlayer.PlayerGui.CollectionGui.CollectionFrame.ScrollingFrame
+
+-- buy roll button 
+workspace.Plots.Plot_1.CrateSpawn.CrateFolder.Cheese.Handle.ProximityPrompt
+
+
+-- auto dupe --

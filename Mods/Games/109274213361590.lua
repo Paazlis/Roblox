f5a007b61c2330cabea2c164864ed77cedc88c9a
Paazlis <paazlismaswa@gmail.com>
2026-07-21 08:20:00 +0700

    Merge an Army V1

---
+++ Mods/Games/109274213361590.lua
@@ -0,0 +1,9 @@
+workspace.Prefabs.TroopVisuals.Troop1_Visual -- OwnerUserId and MaxHealth
+workspace.ScriptableObjects.Plots:GetChildren()[8].MergeArea
+
+
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrade.View.Upgrades.SpawnTier.Background.Title
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrade.View.Upgrades.SpawnTier.Background.ButtonContainer.Purchase.Container.Button
+
+game:GetService("Players").LocalPlayer.PlayerGui.Rebirth.View.UpgradeProgress.Fill
+game:GetService("Players").LocalPlayer.PlayerGui.Rebirth.View.ButtonContainer.Rebirth.Container.Button

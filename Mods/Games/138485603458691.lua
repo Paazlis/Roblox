796d91c8f0c32deb4fb437b89e66d1d697c75b2d
Paazlis <paazlismaswa@gmail.com>
2026-07-22 12:22:00 +0700

    Dig for Dinos V1

---
+++ Mods/Games/138485603458691.lua
@@ -0,0 +1,19 @@
+game:GetService("Players").LocalPlayer.PlayerGui.RebirthGui.RebirthFrame.Main.Frame.BuyButton_BufferParent.BuyButton
+game:GetService("Players").LocalPlayer.PlayerGui.RebirthGui.RebirthFrame.Main.Frame.Requirements.CashRequired.RebirthProgressBar_Bar
+game:GetService("Players").LocalPlayer.PlayerGui.RebirthGui.RebirthFrame.Main.MaxRebirths
+
+game:GetService("Players").LocalPlayer.PlayerGui.RebirthGui.RebirthFrame.Main.Frame.Requirements.DinosRequired
+game:GetService("Players").LocalPlayer.PlayerGui.RebirthGui.RebirthFrame.Main.Frame.Requirements.DinosRequired:GetChildren()[4].CheckMark.Visible == true
+
+
+-- auto upgrade --
+game:GetService("Players").LocalPlayer.PlayerGui.UpgradesGui.UpgradesFrame.Main.Rows
+game:GetService("Players").LocalPlayer.PlayerGui.UpgradesGui.UpgradesFrame.Main.Rows.Upgrades_Entry_power.Upgrades_Entry_CurrentLevel
+game:GetService("Players").LocalPlayer.PlayerGui.UpgradesGui.UpgradesFrame.Main.Rows.Upgrades_Entry_power.Upgrades_Entry_WarningLabel
+game:GetService("Players").LocalPlayer.PlayerGui.UpgradesGui.UpgradesFrame.Main.Rows.Upgrades_Entry_jetpackspeed.Upgrades_Entry_CurrentLevel
+game:GetService("Players").LocalPlayer.PlayerGui.UpgradesGui.UpgradesFrame.Main.Rows.Upgrades_Entry_power.UpgradeTiers
+game:GetService("Players").LocalPlayer.PlayerGui.UpgradesGui.UpgradesFrame.Main.Rows.Upgrades_Entry_power.UpgradeTiers.Upgrades_Entry_Tier_1.Upgrades_Entry_Tier_BuyButton
+
+
+-- home --
+game:GetService("Players").LocalPlayer.PlayerGui.HudGuiSafe.Frame.Top.Top.TopButtons.HomeButton_BufferParent.HomeButton

0219c6b1b1cd8ffe14a789093f168b6cd4632f8d
Paazlis <paazlismaswa@gmail.com>
2026-07-22 20:27:49 +0700

    +1 Pickaxe Swing Escape V1

---
+++ Mods/Games/82554996468034.lua
@@ -0,0 +1,18 @@
+-- auto rebirth --
+game:GetService("Players").LocalPlayer.PlayerGui.Game.Rebirths
+game:GetService("Players").LocalPlayer.PlayerGui.Game.Rebirths.Bar.Progress
+game:GetService("Players").LocalPlayer.PlayerGui.Game.Rebirths.Rebirth
+
+-- auto win --
+workspace.Map2.Stages
+workspace.Worlds.World2.DefaultWinPads:GetChildren()[6].HitBox
+
+workspace.Map.Stages
+workspace.Map2
+workspace.Worlds.World2.DefaultWinPads
+
+
+-- buy trail --
+game:GetService("Players").LocalPlayer.PlayerGui.Game.Trails.Main.ScrollingFrame.Green.Buy
+
+game:GetService("Players").LocalPlayer.PlayerGui.Game.Trails.Main.ScrollingFrame.Green.Buy.Frame.Visible == true

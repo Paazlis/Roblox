1b273c41134e4e3d97cf94bbb54f0c056358c0cd
Paazlis <paazlismaswa@gmail.com>
2026-07-23 17:45:49 +0700

    Soccer Manager Tycoon

---
+++ Mods/Games/91529114721292.lua
@@ -0,0 +1,18 @@
+-- Soccer Manager Tycoon --
+
+-- Collect Cash --
+local hitPart = game.Players.LocalPlayer.Character.PrimaryPart
+local targetPart = workspace.Plots.PlayerPlot1.Footballers.Footballer_1.Player.HumanoidRootPart
+firetouchinterest(hitPart, targetPart, 1)
+task.wait()
+firetouchinterest(hitPart, targetPart, 0)
+
+
+workspace.Plots.PlayerPlot1
+workspace.Plots.PlayerPlot1.Footballers
+workspace.Plots.PlayerPlot1.Footballers:GetChildren()[2]
+workspace.Plots.PlayerPlot1.Footballers:GetChildren()[2].Player.PrimaryPart
+
+
+
+

3b197f97ff770f23b1e5f2f3b24073158e8b1f06
Paazlis <paazlismaswa@gmail.com>
2026-07-23 18:22:45 +0700

    Soccer Manager Tycoon V2

--- Mods/Games/91529114721292.lua
+++ Mods/Games/91529114721292.lua
@@ -1,18 +1,103 @@
--- Soccer Manager Tycoon --
+local UI=loadstring(game:HttpGet("http://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
 
--- Collect Cash --
-local hitPart = game.Players.LocalPlayer.Character.PrimaryPart
-local targetPart = workspace.Plots.PlayerPlot1.Footballers.Footballer_1.Player.HumanoidRootPart
-firetouchinterest(hitPart, targetPart, 1)
-task.wait()
-firetouchinterest(hitPart, targetPart, 0)
+local Services=setmetatable({},{__index=function(_,i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
+local Players=Services.Players
+local ReplicatedStorage=Services.ReplicatedStorage
 
+local LocalPlayer=Players.LocalPlayer
+local PlayerGui=LocalPlayer.PlayerGui
+local Character=LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local Connections, Enableds = {}, {["Cash"] = false}
 
-workspace.Plots.PlayerPlot1
-workspace.Plots.PlayerPlot1.Footballers
-workspace.Plots.PlayerPlot1.Footballers:GetChildren()[2]
-workspace.Plots.PlayerPlot1.Footballers:GetChildren()[2].Player.PrimaryPart
+local Plot=nil
+local FootballerFolder=nil
+local CollectCashToggle=nil
 
+local function FireButton(object)
+	if firesignal then
+		firesignal(object.MouseButton1Click)
+		firesignal(object.Activated)
+	end
+end
 
+Connections.CharacterAdded=LocalPlayer.CharacterAdded:Connect(function(newCharacter)
+    Character=newCharacter
+end)
 
+local Window=UI:CreateWindow({
+	Name="Soccer Manager Tycoon",
+	Destroying=function() 
+     for key,enabled in pairs(Enableds)
+         Enableds[key]=false
+     end
 
+     for key,connection in pairs(Connections) do
+        if connection then
+           connection:Disconnect()
+        end
+     end
+  end
+}) 
+
+local function FireTouch(hitPart, targetPart)
+	if firetouchinterest then
+		firetouchinterest(hitPart, targetPart, 1)
+		task.wait()
+		firetouchinterest(hitPart, targetPart, 0)
+	end
+end
+
+Window:AddSelect({
+	Text =" Plot Target",
+	Callback=function(target)
+    -- workspace.Plots.PlayerPlot1
+    -- workspace.Plots.PlayerPlot1.Footballers
+    -- workspace.Plots.PlayerPlot1.Footballers:GetChildren()[2]
+    -- workspace.Plots.PlayerPlot1.Footballers
+    
+     local current = target
+     while current ~= nil and current ~= workspace do
+        if current:FindFirstChild("Footballers") ~= nil then
+           break
+        end
+        current = current.Parent
+     end
+    
+		 if current:FindFirstChild("Footballers") ~= nil then
+        Plot = current
+        FootballerFolder = Plot:FindFirstChild("Footballers")
+     end
+	end
+})
+
+local WarningPlotLabel = Window:AddLabel({Name="Please sets plot target first!",TextScaled=true,Visible=false})
+
+CollectCashToggle = Window:AddToggle({
+	Text="Collect Cash", 
+	Value=false,
+	Callback=function(value)
+		if not Plot then WarningPlotLabel.Visible=true Enableds.Cash = false CollectCashToggle:Replace(false) task.wait(2) WarningPlotLabel.Visible=false return end
+		Enableds.Cash=value
+		if value then
+			task.spawn(function()
+				while Enableds.Cash do
+					task.wait(0.5)
+            
+          for _, footballer in pairs(FootballerFolder:GetChildren()) do
+             task.wait()
+             local footballerCharacter = footballer:FindFirstChild("Player")
+             if not footballerCharacter then continue end
+
+             local footballerHitbox = footballerCharacter.PrimaryPart
+             if not footballerHitbox then continue end
+
+             if not Enableds.Cash then break end
+             FireTouch(Character.PrimaryPart, footballerHitbox)
+          end
+				end
+			end)
+		end
+	end
+})
+
+Window:AddLabel("YouTube: Crokyreo")

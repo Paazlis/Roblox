51e2cdf802a35185197415667f5c151a3f336e84
Paazlis <paazlismaswa@gmail.com>
2026-07-24 13:01:12 +0700

    +1 Fat Evolution V2

--- Mods/Games/112167159515934.lua
+++ Mods/Games/112167159515934.lua
@@ -1,3 +1,17 @@
 -- Wins Part --
 CFrame.new(1481.20544, -986.76593, 88.7498932, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 
+game:GetService("Players").LocalPlayer.PlayerGui.FatGui:GetChildren()[10].Rebirth
+
+game:GetService("Players").LocalPlayer.PlayerGui.FatGui:GetChildren()[10].LvlBar.Progress
+
+
+-- buy trails --
+-- ImageButton
+game:GetService("Players").LocalPlayer.PlayerGui.FatGui:GetChildren()[38].Titles.effects.ScrollingFrame.Ghost
+game:GetService("Players").LocalPlayer.PlayerGui.FatGui:GetChildren()[38].Titles.DisplayFrame.buyupgrade.Visible == true
+
+
+-- buy boost --
+game:GetService("Players").LocalPlayer.PlayerGui.FatGui.UpgradeHolder.Upgrades.ScrollingFrame.FatSpeed.buyupgrade
+game:GetService("Players").LocalPlayer.PlayerGui.FatGui.UpgradeHolder.Upgrades.ScrollingFrame.FatStrength.buyupgrade

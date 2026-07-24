0067b15a50ab63192ff22185d1e1b51596570654
Paazlis <paazlismaswa@gmail.com>
2026-07-24 11:12:49 +0700

    Build a base RNG V1

---
+++ Mods/Games/99108783264633.lua
@@ -0,0 +1,6 @@
+game:GetService("Players").LocalPlayer.PlayerGui.SkillTree.Main.Content.Available
+game:GetService("Players").LocalPlayer.PlayerGui.SkillTree.Main.Content.Available.PriceFrame.Price.TextColor3 == Color3.new(255, 255, 255)
+
+game:GetService("Players").LocalPlayer.PlayerGui.SkillTree.Main.Content.Hidden
+game:GetService("Players").LocalPlayer.PlayerGui.SkillTree.Main.Content.Owned
+game:GetService("Players").LocalPlayer.PlayerGui.SkillTree.Main.Content.SectionLink

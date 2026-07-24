50cf48026f427ad16424bf80836384f49cd0f773
Paazlis <paazlismaswa@gmail.com>
2026-07-24 14:01:03 +0700

    +1 Moonwalk Escape V1

---
+++ Mods/Games/108775830475023.lua
@@ -0,0 +1,9 @@
+workspace.SectionAwardParts.World2
+ workspace.SectionAwardParts workspace.World3
+
+
+-- auto rebirth -- game:GetService("Players").LocalPlayer.PlayerGui.Main.Rebirth.Rebirth
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Rebirth.LevelBar.Fill
+
+-- touch pad --
+workspace.StepsPlatforms["5"].Pad

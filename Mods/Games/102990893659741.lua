116ed8c3300301cf844448fd46692c43a3df638f
Paazlis <paazlismaswa@gmail.com>
2026-07-24 21:34:50 +0700

    Brainblast a Lucky Block V1

---
+++ Mods/Games/102990893659741.lua
@@ -0,0 +1,9 @@
+-- auto train --
+game:GetService("Players").LocalPlayer.PlayerGui.HUD.MainFrame.X2Frame.X2Button
+local DoubleTrainButton = PlayerGui:QueryDecendants("Frame#HUD > Frame#MainFrame > Frame#X2Frame > X2Button")[1]
+
+-- auto rebirth --
+
+-- auto farm --
+
+-- collect cash --

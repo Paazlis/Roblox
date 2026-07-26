049a8f39bb91c265b78c4393d0afa31255e263c9
Paazlis <paazlismaswa@gmail.com>
2026-07-26 18:20:08 +0700

    +1 Speed Super Hero Escape V1

---
+++ Mods/Games/92937726498067.lua
@@ -0,0 +1,18 @@
+print(workspace.WinPads.NormalPads["15"]:GetPivot())workspace.WinPads.NormalPads
+
+-- rebirth --
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Frames.Rebirth
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Frames.Rebirth.RebirthButton
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Frames.Rebirth.RebirthProgressBar.Main
+
+-- claim speed pads --
+workspace.SpeedMultiplierPads
+workspace.SpeedMultiplierPads.Tier1.Hitbox
+
+-- claim stamina pads --
+workspace.HeroPads.Tier1
+workspace.HeroPads.Tier1.Hitbox
+
+-- buy trails --
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Frames.Trails.ScrollingFrame
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Frames.Trails.ScrollingFrame.FlamingTrail.Wins

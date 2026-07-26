3b9fe00b3e3f5af800e74d21f88bbab3a50a7943
Paazlis <paazlismaswa@gmail.com>
2026-07-26 12:50:09 +0700

    Auto Click V14

--- Mods/Universal/AutoClick.lua
+++ Mods/Universal/AutoClick.lua
@@ -18,18 +18,21 @@
 local UI = loadstring(game:HttpGet("http://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
 
 -- SERVICES --
-local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
+local Services=setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
+local Players=Services.Players
 local RunService=Services.RunService
 local UserInputService=Services.UserInputService
 local VirtualInputManager=Services.VirtualInputManager
 
 -- VARIABLES --
+local PlayerGui=Players.LocalPlayer:FindFirstChildOfClass("PlayerGui")
+
 local SaveEnableds,Enableds={},{["Click"]=false,["HoldClick"]=false}
 local HoldDuration=2
 local ClickSpeed=0.01
 local ClickThread=nil
 local ClickPoint=UserInputService:GetMouseLocation()
-
+local LayerCollectorType="Default"
 local Window=UI:CreateWindow({Name="Maswa Clicker",Destroying=function()
 	 task.cancel(ClickThread)
 	 for key, lenabled in pairs(Enableds) do
@@ -37,7 +40,26 @@ local Window=UI:CreateWindow({Name="Maswa Clicker",Destroying=function()
 	 end
 end})
 
+Window:AddLabel("Layer Collector")
+
+Window:AddSelector({
+	Options={"Default","PlayerGui","Workspace"},
+	NoCap=true,
+	Callback=function(value)
+		LayerCollectorType=value
+	end
+})
 local Status=Window:AddLabel({Name="Point: "..tostring(ClickPoint)})
+
+local function GetLayerCollector()
+	local layerCollector = game
+	if LayerCollectorType == "PlayerGui" then
+		layerCollector = PlayerGui
+	elseif LayerCollectorType == "Workspace" then
+		layerCollector = workspace
+	end
+	return layerCollector
+end
 
 local function FastWait(duration)
 	if not duration then return RunService.RenderStepped:Wait() end
@@ -47,20 +69,20 @@ local function FastWait(duration)
 end
 
 local function SendClick(x,y)
-	VirtualInputManager:SendMouseButtonEvent(x,y,0,true,game,0)
+	VirtualInputManager:SendMouseButtonEvent(x,y,0,true,GetLayerCollector(),0)
 	FastWait()
-	VirtualInputManager:SendMouseButtonEvent(x,y,0,false,game,0)
+	VirtualInputManager:SendMouseButtonEvent(x,y,0,false,GetLayerCollector(),0)
 end
 
 local function SendHoldClick(x, y, duration)
     -- 1. Tekan dan tahan mouse (isDown = true)
-    VirtualInputManager:SendMouseButtonEvent(x,y,0,true,game,0)
+    VirtualInputManager:SendMouseButtonEvent(x,y,0,true,GetLayerCollector(),0)
     
     -- 2. Tahan selama durasi yang diinginkan (misal: 2 detik)
     task.wait(duration)
     
     -- 3. Lepaskan kembali mouse (isDown = false)
-    VirtualInputManager:SendMouseButtonEvent(x,y,0,false,game,0)
+    VirtualInputManager:SendMouseButtonEvent(x,y,0,false,GetLayerCollector(),0)
 end
 
 -- AUTOCLICK FUNCTION --

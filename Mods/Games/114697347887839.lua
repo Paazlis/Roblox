966c737ae419fc8b018caa51ff0911839ee096ff
Paazlis <paazlismaswa@gmail.com>
2026-07-27 15:17:22 +0700

    +1 Speed Monkey Escape V1

---
+++ Mods/Games/114697347887839.lua
@@ -0,0 +1,19 @@
+-- Auto Win --
+game:GetService("Players").LocalPlayer.Data.LastCheckpoint.Value -- SpawnPoint Instance 
+game:GetService("Players").LocalPlayer.Data.World.Value -- 1 number
+
+
+workspace.Map.Checkpoints
+workspace.Map.Checkpoints.World1.Checkpoint1.SpawnPoint
+
+workspace.Map.World1.Stages.Stage1.NormalWin.Button
+workspace.Map.World2.Stages.Stage1.NormalWin.Button
+
+-- Auto Rebirth --
+ game:GetService("Players").LocalPlayer.PlayerGui.Main.UIs.Rebirth
+game:GetService("Players").LocalPlayer.PlayerGui.Main.UIs.Rebirth.Level.CanvasGroup.Bar
+game:GetService("Players").LocalPlayer.PlayerGui.Main.UIs.Rebirth.Buttons.Rebirth
+
+-- Equip Best Rail --
+workspace.Map.World1.Upgrades
+workspace.Map.World1.Upgrades.Button9 -- Upgrade number attribute or tonumber(child.Name)

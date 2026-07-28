95ad66eb27a7902b879030ab0635fde8e058d9b5
Paazlis <paazlismaswa@gmail.com>
2026-07-28 19:02:02 +0700

    +1 Grow Per Step WIP 1

---
+++ Mods/Games/96142137828854.lua
@@ -0,0 +1,11 @@
+-- Wins Farm --
+-- Cari object benama "Win" di "WinPads"
+workspace.WinPads.Win16.TouchPart
+
+-- Auto Step --
+
+
+-- Auto Rebirth --
+game:GetService("Players").LocalPlayer.PlayerGui.Menus.RebirthFrame
+game:GetService("Players").LocalPlayer.PlayerGui.Menus.RebirthFrame.ProgressBar.Fill
+game:GetService("Players").LocalPlayer.PlayerGui.Menus.RebirthFrame.RebirthConfirmButton

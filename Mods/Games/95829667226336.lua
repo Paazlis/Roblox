9ac9501236c9bd0f0d3e4813801b4daf90a79d98
Paazlis <paazlismaswa@gmail.com>
2026-07-29 10:15:46 +0700

    Dumpling Stars V1

---
+++ Mods/Games/95829667226336.lua
@@ -0,0 +1,7 @@
+-- Collect Cash --
+workspace.Plots["1"] -- Owner number 
+workspace.Plots["1"]["1"].Containers["10"]["10"].Collection.CollectionPad
+
+-- Auto Fight --
+workspace.Bosses 
+workspace.Bosses.boss1.Humanoid

38f7feb5d0918bca6b18eaf19b00925a17f7e490
Paazlis <paazlismaswa@gmail.com>
2026-07-29 20:34:36 +0700

    Speedsters Infinite V1

---
+++ Mods/Games/79658956070105.lua
@@ -0,0 +1,7 @@
+game:GetService("Players").LocalPlayer.PlayerGui.MainHUD.Menus.SpeedstersFrame.ScrollingFrame
+game:GetService("Players").LocalPlayer.PlayerGui.MainHUD.Menus.SpeedstersFrame.ScrollingFrame.Frame2.EquipButton.BackgroundColor3 == Color3.new(210, 160, 40) or Color3.new(30, 160, 90) for equip
+
+
+game:GetService("Players").LocalPlayer.PlayerGui.MainHUD.Menus.RebirthFrame
+game:GetService("Players").LocalPlayer.PlayerGui.MainHUD.Menus.RebirthFrame.BarBG.BarFill
+game:GetService("Players").LocalPlayer.PlayerGui.MainHUD.Menus.RebirthFrame.RebirthButton

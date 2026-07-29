56b05cb1ac1bf8492e11540506c7a1cd2b103dc6
Paazlis <paazlismaswa@gmail.com>
2026-07-29 10:41:58 +0700

    Dumpling Stars V2

--- Mods/Games/95829667226336.lua
+++ Mods/Games/95829667226336.lua
@@ -1,7 +1,14 @@
+-- Auto Fight --
+workspace.Bosses 
+workspace.Bosses.boss1.Humanoid
+
+-- Auto Train --
+game:GetService("Players").LocalPlayer.PlayerGui.SpeedEffect.LeftContainer.Currency.Speed.x2Speed
+game:GetService("Players").LocalPlayer.PlayerGui.SpeedEffect.LeftContainer.Currency.Speed.x2Speed.Visible == true
+
 -- Collect Cash --
 workspace.Plots["1"] -- Owner number 
 workspace.Plots["1"]["1"].Containers["10"]["10"].Collection.CollectionPad
 
--- Auto Fight --
-workspace.Bosses 
-workspace.Bosses.boss1.Humanoid
+-- YouTube: Tora IsMe
+-- YouTube: Crokyreo

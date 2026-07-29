f61a7474503828152bb34a3e66b9773e9cb1cfb0
Paazlis <paazlismaswa@gmail.com>
2026-07-29 16:54:19 +0700

    Deposit Simulator V4 Completed

--- Mods/Games/101607790076338.lua
+++ Mods/Games/101607790076338.lua
@@ -1,30 +1,27 @@
 local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
-local Utility = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Utility/init.luau"))()
 
 local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
 local Players = Services.Players
 local ReplicatedStorage = Services.ReplicatedStorage
 
 local LocalPlayer = Players.LocalPlayer
-local DepositEnabled = false
+local Enableds = {["Deposit"] = false}
 
--- Deposit Function --
 local function AutoDeposit()
-	if DepositEnabled then
+	if Enableds.Deposit then
 		task.spawn(function()
-			while DepositEnabled do
-				task.wait(0.5)
+			while Enableds.Deposit do
 				ReplicatedStorage.RemoveLastBottle:FireServer()
+				task.wait(0.5)
 			end
 		end)
 	end
 end
 
--- Main UI --
 local Window = UI:CreateWindow({
 	Name = "Deposit Simulator", 
 	Destroying = function()
-		DepositEnabled = false
+		Enableds.Deposit = false
 	end
 })
 

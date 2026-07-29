480a359c463e09a11482dc9da2c0101b819d3801
Paazlis <paazlismaswa@gmail.com>
2026-07-29 13:40:43 +0700

    Airport Tycoon V1

---
+++ Mods/Games/138486812747835.lua
@@ -0,0 +1,9 @@
+-- Collect Button --
+workspace.AirportTycoonClientRuntime["2"].Buttons
+workspace.AirportTycoonClientRuntime["2"].Buttons.Trashcan2Button
+workspace.AirportTycoonClientRuntime["2"].Buttons.Trashcan2Button.Touch
+
+-- Auto Rebirth --
+game:GetService("Players").LocalPlayer.PlayerGui.UI.Menus.Rebirth
+game:GetService("Players").LocalPlayer.PlayerGui.UI.Menus.Rebirth.Rebirth
+game:GetService("Players").LocalPlayer.PlayerGui.UI.Menus.Rebirth.Rebirth.Header.Text =="NOT READY"

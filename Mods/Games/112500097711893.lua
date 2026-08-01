09f57627fd18f213fc96c2279883f01431e0b077
Paazlis <paazlismaswa@gmail.com>
2026-08-01 20:11:29 +0700

    Link a Brainrots V4 Completed

--- Mods/Games/112500097711893.lua
+++ Mods/Games/112500097711893.lua
@@ -1,14 +1,21 @@
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/main/Packages/Sampluy/init.luau"))()
 
 local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
 local Players = Services.Players
 local ReplicatedStorage = Services.ReplicatedStorage
 
 local LocalPlayer = Players.LocalPlayer
-local PlayerGui = LocalPlayer.PlayerGui
+local PlayerGui = LocalPlayer:FindFirstChildOfClass("PlayerGui")
+local Backpack = LocalPlayer:FindFirstChildOfClass("Backpack")
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 
-local CashEnabled, TrainEnabled, RebirthEnabled, Farming = false, false, false, false, false
-local TrainConnection = nil
+local Packets = {}
+local Enableds, Connections = {Cash = false, Train = false, Rebirth = false, Farm = false}, {}
+local Plot = nil
+
+Connections.CharacterAdded = LocalPlayer.CharacterAdded:Connect(function(newCharacter)
+	Character = newCharacter
+end)
 
 local function GetPlot()
 	local plots = workspace:FindFirstChild("Plots")
@@ -16,146 +23,178 @@ local function GetPlot()
 
 	for _, base in pairs(plots:GetChildren()) do
 		local ownerId = base:GetAttribute("Owner")
-        if tostring(ownerId) == tostring(LocalPlayer.UserId) then
-            return base
-        end
+		if tostring(ownerId) == tostring(LocalPlayer.UserId) then
+			return base
+		end
 	end
 
 	return nil
 end
 
-local Plot = GetPlot()
-
-local function FireButton(object)
+local function FireButton(button)
 	if firesignal then
-		firesignal(object.MouseButton1Click)
-		firesignal(object.Activated)
+		firesignal(button.MouseButton1Click)
+		firesignal(button.Activated)
 	end
 end
 
 -- Train Function --
 local function TrainAdded(child)
-   if child.Name:lower() == "doublebutton" and TrainEnabled then
-      pcall(FireButton, child)
-   end
+	if child.Name:lower() == "doublebutton" and Enableds.Train then
+		FireButton(child)
+	end
 end
 
-local function AutoTrain()
-    if TrainConnection then TrainConnection:Disconnect() TrainConnection = nil end
-    if TrainEnabled then
-        task.spawn(function()
-            while TrainEnabled do
-                task.wait(1)
-                local gym = LocalPlayer.Backpack:FindFirstChild("Gym")
-                if gym then
-                    pcall(function()
-                        LocalPlayer.Character.Humanoid:EquipTool(gym)
-                    end)
-                end
-               	ReplicatedStorage.Remotes.doubleStrength:FireServer()
-            end
-        end)
+local function  HandleTrain()
+	if Connections.Train then Connections.Train:Disconnect() Connections.Train = nil end
+	if Enableds.Train then
+		task.spawn(function()
+			Packets.SendDoubleStrength = Packets.SendDoubleStrength or ReplicatedStorage.Remotes.doubleStrength
+			while Enableds.Train do
+				local gym = Backpack:FindFirstChild("Gym")
+				if gym then
+					local humanoid = Character:FindFirstChildOfClass("Humanoid")
+					if humanoid and Enableds.Train then
+						humanoid:EquipTool(gym)
+					end
+				end
+				if Enableds.Train then
+					Packets.SendDoubleStrength:FireServer()
+				end
+				task.wait(0.5)
+			end
+		end)
 
-        local frame = PlayerGui.Main
-        TrainConnection = frame.ChildAdded:Connect(TrainAdded)
-        for _, child in pairs(frame:GetChildren()) do
-		   TrainAdded(child)
-        end
-    end
+		local frame = PlayerGui:FindFirstChild("Main")
+		if not frame then return end
+		
+		Connections.Train = frame.ChildAdded:Connect(TrainAdded)
+		for _, child in pairs(frame:GetChildren()) do
+			if not Enableds.Train then break end
+			TrainAdded(child)
+		end
+	end
 end
 
--- Farming Function --
-local function AutoFarming()
-    if Farming then
-        task.spawn(function()
-            while Farming do
-                task.wait(0.5)
-                task.spawn(function()
-                    ReplicatedStorage.Remotes.OnCast:InvokeServer(1)
-                    ReplicatedStorage.Remotes.StartRun:InvokeServer()
-                    ReplicatedStorage.Remotes.FinishRun:InvokeServer(true)
-                end)
-            end
-        end)
-    end
+-- Farm Function --
+local function HandleFarm()
+	if Enableds.Farm then
+		task.spawn(function()
+			Packets.OnCast = Packets.OnCast or ReplicatedStorage.Remotes.OnCast
+			Packets.StartRun = Packets.StartRun or ReplicatedStorage.Remotes.StartRun
+			Packets.FinishRun = Packets.FinishRun or ReplicatedStorage.Remotes.FinishRun
+			
+			while Enableds.Farm do
+				task.spawn(function()
+					Packets.OnCast:InvokeServer(1)
+					Packets.StartRun:InvokeServer()
+					Packets.FinishRun:InvokeServer(true)
+				end)
+				
+				task.wait(0.5)
+			end
+		end)
+	end
 end
 
 -- Collect Cash Function --
-local function AutoCash()
-    if CashEnabled then
-        task.spawn(function()
-            while CashEnabled do
-                task.wait(0.5)
-                Plot = (Plot ~= nil and Plot.Parent) and Plot or GetPlot()
-                if Plot then
-                    local slots = Plot:FindFirstChild("Slots")
-                    if slots then
-                        for _, slot in pairs(slots:GetChildren()) do
-                           ReplicatedStorage.Remotes.Claim:InvokeServer(tonumber(slot.Name) or 1)
-                        end
-                    end
-                end
-            end
-        end)
-    end
+local function HandleCash()
+	if Enableds.Cash then
+		task.spawn(function()
+			Packets.ClaimCash = Packets.ClaimCash or ReplicatedStorage.Remotes.Claim
+			
+			while Enableds.Cash do
+				Plot = (Plot ~= nil and Plot.Parent) and Plot or GetPlot()
+				if Plot then
+					local slots = Plot:FindFirstChild("Slots")
+					if slots then
+						for _, slot in pairs(slots:GetChildren()) do
+							task.wait()
+							if not Enableds.Cash then break end
+							local slotTier = tonumber(slot.Name) or 1
+							if not slotTier then continue end
+							Packets.ClaimCash:InvokeServer(slotTier)
+						end
+					end
+				end
+				
+				task.wait(0.5)
+			end
+		end)
+	end
 end
 
 -- Rebirth Function --
-local function AutoRebirth()
-    if RebirthEnabled then
-        task.spawn(function()
-            while RebirthEnabled do
-                task.wait(5)
-                ReplicatedStorage.Remotes.Rebirth:InvokeServer()
-            end
-        end)
-    end
+local function HandleRebirth()
+	if Enableds.Rebirth then
+		Packets.RequestRebirth = Packets.RequestRebirth or ReplicatedStorage.Remotes.Rebirth
+		task.spawn(function()
+			while Enableds.Rebirth do
+				Packets.RequestRebirth:InvokeServer()
+				task.wait(5)
+			end
+		end)
+	end
 end
 
--- Main UI --
+Plot = GetPlot()
+
 local Window = UI:CreateWindow({
-    Name = "Lick A Brainrots", 
-    Destroying = function()
-        if TrainConnection then TrainConnection:Disconnect() TrainConnection = nil end
-        CashEnabled, TrainEnabled, RebirthEnabled, Farming = false, false, false, false, false
-    end
+	Name = "Lick A Brainrots", 
+	Destroying = function()
+		for key, enabled in pairs(Enableds) do
+			Enableds[key] = false
+		end
+		for key, connection in pairs(Connections) do
+			if connection then
+				connection:Disconnect()
+			end
+		end
+	end
 })
 
 Window:AddToggle({
-    Text = "Auto Farming",
-    Value = false,
-    Callback = function(value)
-        Farming = value
-        AutoFarming()
-    end
+	Text = "Auto Farm",
+	Value = false,
+	Callback = function(value)
+		Enableds.Farm = value
+		HandleFarm()
+	end
 })
 
 Window:AddToggle({
-    Text = "Auto Train",
-    Value = false,
-    Callback = function(value)
-        TrainEnabled = value
-        AutoTrain()
-    end
+	Text = "Auto Train",
+	Value = false,
+	Callback = function(value)
+		Enableds.Train = value
+		HandleTrain()
+	end
 })
 
 Window:AddToggle({
-    Text = "Collect Cash",
-    Value = false,
-    Callback = function(value)
-        CashEnabled = value
-        AutoCash()
-    end
+	Text = "Collect Cash",
+	Value = false,
+	Callback = function(value)
+		Enableds.Cash = value
+		HandleCash()
+	end
 })
 
 Window:AddToggle({
-    Text = "Auto Rebirth",
-    Value = false,
-    Callback = function(value)
-        RebirthEnabled = value
-        AutoRebirth()
-    end
+	Text = "Auto Rebirth",
+	Value = false,
+	Callback = function(value)
+		Enableds.Rebirth = value
+		HandleRebirth()
+	end
 })
 
-Window:AddLabel("YouTube: Crokyreo")
-Window:AddLabel("YouTube: vaehz")
+Window:AddLabel({
+	Text = "YouTube: Crokyreo",
+	TextColor3 = Color3.fromRGB(255, 255, 255)
+})
+
+Window:AddLabel({
+	Text = "YouTube: vaehz",
+	TextColor3 = Color3.fromRGB(255, 255, 255)
+})

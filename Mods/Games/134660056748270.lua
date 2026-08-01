149c5d859bf1a8894459909ee7448084d8d7fb69
Paazlis <paazlismaswa@gmail.com>
2026-08-02 00:01:18 +0700

    +1 Speed Per Click V6

--- Mods/Games/134660056748270.lua
+++ Mods/Games/134660056748270.lua
@@ -6,6 +6,8 @@ local ReplicatedStorage = Services.ReplicatedStorage
 
 local LocalPlayer = Players.LocalPlayer
 local PlayerGui = LocalPlayer:FindFirstChildOfClass("PlayerGui")
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 
 local Enableds, Connections = {Click = false, ThrowClick = false, Farm = false, Rebirth = false}, {}
 local Packets = {
@@ -15,12 +17,24 @@ local Packets = {
 }
 local RebirthScroll = PlayerGui:QueryDescendants("#Main > #Frames > #Rebirth > #Rebirths")[1]
 local RebirthCache = {}
+local AdminSpawnsFolder = workspace:FindFirstChild("AdminSpawns")
+local RebirthAddThread = nil
+
+Connections.CharacterAdded = LocalPlayer.CharacterAdded:Connect(function(newCharacter)
+	Character = newCharacter
+end)
 
 local function FireButton(button)
-   if firesignal then
-	  firesignal(button.Activated)
-	  firesignal(button.MouseButton1Click)
-   end
+	if firesignal then
+		firesignal(button.Activated)
+		firesignal(button.MouseButton1Click)
+	end
+end
+
+local function FirePrompt(prompt)
+	if fireproximityprompt  then
+		fireproximityprompt(prompt)
+	end
 end
 
 local function HandleClick()
@@ -47,7 +61,7 @@ end
 
 local function HandleFarm()
 	if not Enableds.Farm then return end
-	
+
 	task.spawn(function()
 		while Enableds.Farm do
 			Packets.ThrowReward:FireServer(math.random(99999999, 99999999999))
@@ -56,28 +70,27 @@ local function HandleFarm()
 	end)
 end
 
-local RebirthAddThread = nil
 
 local function RebirthAdded(child)
 	if not Enableds.Rebirth then return end
-	
+
 	if child:IsA("Frame") then
 		if RebirthAddThread and coroutine.status(RebirthAddThread) ~= "dead" then
 			task.cancel(RebirthAddThread)
 			RebirthAddThread = nil
 		end
-		
+
 		local childName = child.Name
 		local confirmButton = child:FindFirstChild("Confirm")
-		
+
 		if not confirmButton then 
 			return 
 		end
-		
+
 		if childName == "MaxRebirthButton" or childName == "Padding" or childName == "RebirthUpgradeButton" then
-           return
+			return
 		end
-		
+
 		table.insert(RebirthCache, {
 			Name = childName,
 			Tier = child.LayoutOrder,
@@ -85,11 +98,11 @@ local function RebirthAdded(child)
 		})
 
 		local lastLength = #RebirthCache
-		
+
 		RebirthAddThread = task.delay(2, function()
 			if #RebirthCache == lastLength then
 				task.sort(RebirthCache, function(a, b)
-                    return a.Tier < b.Tier
+					return a.Tier < b.Tier
 				end)
 			end
 		end)
@@ -101,33 +114,96 @@ local function HandleRebirth()
 		task.cancel(RebirthAddThread)
 		RebirthAddThread = nil
 	end
- 
+
 	if Connections.RebirthAdded then Connections.RebirthAdded:Disconnect() Connections.RebirthAdded = nil end
 	table.clear(RebirthCache)
-	
+
 	if not Enableds.Rebirth then return end
-	
+
 	Connections.RebirthAdded = RebirthScroll.ChildAdded:Connect(RebirthAdded)
-	
+
 	for _, child in ipairs(RebirthScroll:GetChildren()) do
-       if not Enableds.Rebirth then return end
-	   RebirthAdded(child)
+		if not Enableds.Rebirth then return end
+		RebirthAdded(child)
 	end
 
 	task.spawn(function()
 		while Enableds.Rebirth do
 			for _, rebirthStats in ipairs(RebirthCache) do
-			   task.wait()
-			   if not Enableds.Rebirth then break end
-			   local confirmButton = rebirthStats.ConfirmButton
-			   if confirmButton then
-				  FireButton(confirmButton)
-			   end
+				task.wait()
+				if not Enableds.Rebirth then break end
+				local confirmButton = rebirthStats.ConfirmButton
+				if confirmButton then
+					FireButton(confirmButton)
+				end
 			end
 			task.wait(0.5)
 		end
 	end)
 end
+
+--local function HandleAdminItem()
+--	if not Enableds.AdminItem then return end
+	
+--	Connections.AdminItemPromptShow = ProximityPromptService.PromptShown:Connect(function(prompt)
+--		local current = prompt
+		
+--		repeat
+--			if current and current.Parent then
+--				current = current.Parent
+--			end
+--			task.wait()
+--		until current == AdminSpawnsFolder or current == workspace
+		
+--		if current == AdminSpawnsFolder and Enableds.AdminItem then
+--			FirePrompt(prompt)
+--		end
+--	end)
+	
+--	while Enableds.AdminItem do
+--		for _, spawnPart in ipairs(AdminSpawnsFolder:GetChildren()) do
+--			if spawnPart:IsA("BasePart") then
+--				task.wait()
+--				if not Enableds.AdminItem then break end
+				
+--				local prompt = spawnPart:FindFirstChildOfClass("ProximityPrompt")
+				
+				
+--				Character:PivotTo(spawnPart.CFrame)
+--				if key == "AllEnabled" then
+--					continue
+--				end
+
+--				if BahanActives["AllEnabled"] then
+--					active = true
+--				end
+
+--				if active then
+--					local bahanList = BahanInfos[key]
+--					if not bahanList then continue end
+
+--					for _, bahanStats in ipairs(bahanList) do
+--						local spawnPoint = bahanStats.SpawnPoint
+--						local prompt = bahanStats.Prompt
+
+--						if spawnPoint and prompt then
+--							if prompt.Enabled == true and Enableds.Collect then
+--								teleporting = true
+--								Character:PivotTo(spawnPoint.CFrame)
+--								task.wait(0.2)
+--								FirePrompt(prompt)
+--								task.wait(0.1)
+--							end
+--						end
+--					end
+--				end
+--			end
+--		end
+		
+--		task.wait(1)
+--	end
+--end
+
 
 local Window = UI:CreateWindow({
 	Name = "+1 Speed Per Click",
@@ -135,7 +211,7 @@ local Window = UI:CreateWindow({
 		for key, enabled in pairs(Enableds) do
 			Enableds[key] = false
 		end
-		
+
 		for key, connection in pairs(Connections) do
 			if connection then
 				connection:Disconnect()
@@ -143,8 +219,8 @@ local Window = UI:CreateWindow({
 		end
 
 		if RebirthAddThread and coroutine.status(RebirthAddThread) ~= "dead" then
-		   task.cancel(RebirthAddThread)
-		   RebirthAddThread = nil
+			task.cancel(RebirthAddThread)
+			RebirthAddThread = nil
 		end
 
 		table.clear(RebirthCache)
@@ -191,18 +267,19 @@ Window:AddToggle({
 	end
 })
 
-Window:AddButton({
-	Text = "Collect Lucky Block",
-	MethodType = "DebounceClick",
-	Callback = function()
-		
-	end
-})
+--Window:AddToggle({
+--	Text = "Collect Admin Item",
+--	MethodType = "DebounceClick",
+--	Callback = function()
+
+--	end
+--})
 
 Window:AddLabel({
 	Text = "YouTube: Crokyreo",
 	TextColor3 = Color3.fromRGB(255, 255, 255)
 })
+
 
 -- Game Info --
 --[[
@@ -244,6 +321,7 @@ game:GetService("Players").LocalPlayer.PlayerGui.Main.Frames.Rebirth.Best.TextLa
 -- MaxRebirthButton, Padding, RebirthUpgradeButton
 
 -- Collect Lucky Block --
+
 workspace.AdminSpawns.Spawn
 workspace.AdminSpawns.Spawn.Part.AdminEgg_1785602042_508
 

a9d2590343ea37941a38c35bac3f8c12d055e59c
Paazlis <paazlismaswa@gmail.com>
2026-08-01 12:50:19 +0700

    Devil's Market / Pasar Setan V1

---
+++ Mods/Games/108679402300081.lua
@@ -0,0 +1,27 @@
+-- Jasmine Flower / Melati --
+workspace.SpawnBahan.Spawn_Melati.BahanVisual.e -- Transparency == 0
+workspace.SpawnBahan.Spawn_Melati.BahanVisual.Grip.AmbilPrompt
+
+-- Frankincense/ Kemenyan --
+workspace.SpawnBahan.Spawn_Kemenyan
+workspace.SpawnBahan.Spawn_Kemenyan.BahanVisual.Handle.AmbilPrompt 
+
+-- River Crab / Kepiting Sungai --
+workspace.SpawnBahan.Spawn_KepitingSungai.BahanVisual.Grip
+workspace.SpawnBahan.Spawn_KepitingSungai.BahanVisual.Grip.AmbilPrompt
+workspace.SpawnBahan.Spawn_KepitingSungai.BahanVisual.node_0
+
+
+-- Graveyard Mushroom / Jamur Kuburan --
+workspace.SpawnBahan.Spawn_JamurKuburan
+workspace.SpawnBahan.Spawn_JamurKuburan.BahanVisual.Grip.AmbilPrompt
+
+
+-- Carrion Bird / Gagak --
+workspace.SpawnBahan.Spawn_Gagak
+workspace.SpawnBahan.Spawn_Gagak.BahanVisual.defaultMaterial
+workspace.SpawnBahan.Spawn_Gagak.BahanVisual.Grip.AmbilPrompt
+
+-- Incense / Dupa --
+workspace.SpawnBahan.Spawn_Dupa.BahanVisual.Grip.AmbilPrompt
+

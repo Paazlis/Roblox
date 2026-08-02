580e001919a4a5eef2c38fcf6160a5373683c2e5
Paazlis <paazlismaswa@gmail.com>
2026-08-02 08:07:20 +0700

    Airport Tycoon V2

--- Mods/Games/138486812747835.lua
+++ Mods/Games/138486812747835.lua
@@ -1,9 +1,21 @@
 -- Collect Button --
-workspace.AirportTycoonClientRuntime["2"].Buttons
-workspace.AirportTycoonClientRuntime["2"].Buttons.Trashcan2Button
-workspace.AirportTycoonClientRuntime["2"].Buttons.Trashcan2Button.Touch
+workspace.AirportTycoonClientRuntime["1"].Buttons
+workspace.AirportTycoonClientRuntime["1"].Buttons.Trashcan2Button
+workspace.AirportTycoonClientRuntime["1"].Buttons.Trashcan2Button.Touch
+workspace.AirportTycoonClientRuntime["1"].Buttons.NewDividerWall1Button.Config.Costs.Value
+game:GetService("Players").LocalPlayer.leaderstats.Money
 
 -- Auto Rebirth --
 game:GetService("Players").LocalPlayer.PlayerGui.UI.Menus.Rebirth
 game:GetService("Players").LocalPlayer.PlayerGui.UI.Menus.Rebirth.Rebirth
 game:GetService("Players").LocalPlayer.PlayerGui.UI.Menus.Rebirth.Rebirth.Header.Text =="NOT READY"
+
+
+-- Auto Upgrade --
+Security Lane, Gate Desk, Eme
+
+game:GetService("Players").LocalPlayer.PlayerGui.UI.Menus.Upgrades
+
+game:GetService("Players").LocalPlayer.PlayerGui.UI.Menus.Upgrades.Pages.Types.Container.SecurityLanes.Header
+
+game:GetService("Players").LocalPlayer.PlayerGui.UI.Menus.Upgrades.Pages.Types.Container.SecurityLanes.Button

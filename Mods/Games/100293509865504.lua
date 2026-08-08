d8ab0eddc7d5efc4ccd3360ad3fb5e88906f3a4f
Paazlis <paazlismaswa@gmail.com>
2026-08-08 15:47:23 +0700

    Catch 1 Billion Ducks V1 WIP 08-07-2026

---
+++ Mods/Games/100293509865504.lua
@@ -0,0 +1,20 @@
+workspace.Ume.DuckController_Client_11
+workspace.Ume.DuckController_Client_12.RootPart workspace.Ume.DuckController_Client_11.LovPoly
+
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Me
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Me.MeContent.Stats
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Me.MeContent.Stats.Spread.BuyButton
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Me.MeContent.Stats.Spread.S_Button_1.Container
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Me.MeContent.Stats.Spread.S_Button_1.Container.ButtonText.Text
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Me.MeContent.Stats.Spread.S_Button_1.Container:GetChildren()[3].Text ~= "->"
+
+
+
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Dog
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Dog.DogsContent.Stats.Speed.S_Button_1.Container.ButtonText
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Dog.DogsContent.Stats.Speed.BuyButton.S_Button
+
+
+
+
+game:GetService("Players").LocalPlayer.PlayerGui.Upgrades.Widget.Me.MeContent.ButtonContainer.Button_Sell.S_Button

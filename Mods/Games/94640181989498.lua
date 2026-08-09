b17097ecea48df07ef833c549f199e3fc9b2ba57
Paazlis <paazlismaswa@gmail.com>
2026-08-09 07:59:39 +0700

    Grow a Chicken Fighter V1

---
+++ Mods/Games/94640181989498.lua
@@ -0,0 +1,11 @@
+workspace.PitScrap -- child part 
+
+-- need this
+ game:GetService("Players").LocalPlayer.PlayerGui.SideRail.Frame.rebirth
+
+
+-- rebirth check--
+game:GetService("Players").LocalPlayer.PlayerGui.Rebirth.Frame.window.panel.face.content.content.body.confirm.face.label
+
+-- rebirth button --
+game:GetService("Players").LocalPlayer.PlayerGui.Rebirth.Frame.window.panel.face.content.content.body.confirm

f40ebc419e5c84600708d086bb735f9817fb1963
Paazlis <paazlismaswa@gmail.com>
2026-08-11 09:15:06 +0700

    Deposit Simulator V6 Completed 07-04-2026

--- Mods/Games/101607790076338.lua
+++ Mods/Games/101607790076338.lua
@@ -1,27 +1,29 @@
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/main/Packages/Sampluy/init.luau"))()
 
 local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
 local Players = Services.Players
 local ReplicatedStorage = Services.ReplicatedStorage
 
 local LocalPlayer = Players.LocalPlayer
-local Enableds = {["Deposit"] = false}
+local Enableds, Packets = {["Deposit"] = false}, {}
 
 local function HandleDeposit()
-	if Enableds.Deposit then
-		task.spawn(function()
-			while Enableds.Deposit do
-				ReplicatedStorage.RemoveLastBottle:FireServer()
-				task.wait(0.5)
-			end
-		end)
-	end
+	if not Enableds.Deposit then return end
+	Packets.RemoveLastBottle = Packets.RemoveLastBottle or ReplicatedStorage:FindFirstChild("RemoveLastBottle")
+	task.spawn(function()
+		while Enableds.Deposit do
+			Packets.RemoveLastBottle:FireServer()
+			task.wait(1)
+		end
+	end)
 end
 
 local Window = UI:CreateWindow({
 	Name = "Deposit Simulator", 
 	Destroying = function()
-		Enableds.Deposit = false
+		for key, enabled in pairs(Enableds) do
+			Enableds[key] = false
+		end
 	end
 })
 
@@ -34,4 +36,12 @@ Window:AddToggle({
 	end
 })
 
-Window:AddLabel("YouTube: Crokyreo")
+Window:AddLabel({
+   Text = "YouTube: Crokyreo",
+   TextColor3 = Color3.fromRGB(255, 255, 255)
+})
+
+Window:AddLabel({
+   Text = "Date: 07-04-2026",
+   TextColor3 = Color3.fromRGB(255, 255, 255)
+})

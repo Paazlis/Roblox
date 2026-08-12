49b267ba5d35f84b7298c623199d3223ce1c62bb
Paazlis <paazlismaswa@gmail.com>
2026-08-13 02:14:49 +0700

    Flamethrower for Brainrots WIP 1 08-13-2026

---
+++ Mods/Games/130645254478740.lua
@@ -0,0 +1,18 @@
+workspace.Plots.Plot_5.Base.Slots.Platform1.Collector
+workspace.Plots.Plot_5.Floors.Floor_2.Slots
+
+ -- plot OwnerUserId game:GetService("Players").LocalPlayer.PlayerGui.Tabs.Rebirth_V2 
+game:GetService("Players").LocalPlayer.PlayerGui.Tabs.Rebirth_V2.Body.RebirthButtonFrame.Button
+game:GetService("Players").LocalPlayer.PlayerGui.Tabs.Rebirth_V2.Body.ProgressBar.InnerBar
+
+-- Flamethrower Power
+game:GetService("Players").LocalPlayer.PlayerGui.Tabs.FlamethrowerShop.Body.ProductFrame1.PayFrame.PurchaseFrame.PurchaseButton
+
+-- Carry Capacity 
+game:GetService("Players").LocalPlayer.PlayerGui.Tabs.CapacityShop.Body.ProductFrame1.PayFrame.PurchaseFrame.PurchaseButton
+
+-- Speed
+game:GetService("Players").LocalPlayer.PlayerGui.Tabs.UpgradeShop.Body.ProductFrame1.PayFrame.PurchaseFrame.PurchaseButton
+
+-- Sell
+game:GetService("Players").LocalPlayer.PlayerGui.Tabs.SellShop_V2.Body.SellAllButtonFrame.SellAllButton

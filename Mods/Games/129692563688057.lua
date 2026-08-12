2c7e2c166c769c7afaa72aba730a54dbfce645d2
Paazlis <paazlismaswa@gmail.com>
2026-08-13 06:24:38 +0700

    My Dino Life WIP 3

--- Mods/Games/129692563688057.lua
+++ Mods/Games/129692563688057.lua
@@ -1,5 +1,5 @@
 workspace.Food
 
+game:GetService("Players").LocalPlayer.PlayerGui.Experience.Container.Evolve
+game:GetService("Players").LocalPlayer.PlayerGui.Menus.EvolveSelect.Frame.View.EvolveCard
 game:GetService("Players").LocalPlayer.PlayerGui.Menus.EvolveSelect.Frame.View.EvolveCard.Confirm
-
-game:GetService("Players"). localplayer.playergui.experience.container.evolve.Visible == true 

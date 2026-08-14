63002b6b3cac706b7e7a6f62efcdaf7d54afbc28
Paazlis <paazlismaswa@gmail.com>
2026-08-14 17:34:20 +0700

    +1 Spinjitsu Escape WIP 1

---
+++ Games/131910189515331.lua
@@ -0,0 +1,6 @@
+workspace.Map.World1.Stages.Stage15.Won.Win.TouchPart
+FireTouch()
+workspace.Map.World1.Stages
+
+game:GetService("Players").LocalPlayer.PlayerGui.Rebirth.MainFrame game:GetService("Players").LocalPlayer.PlayerGui.Rebirth.MainFrame.ButtonHolder.Rebirth
+game:GetService("Players").LocalPlayer.PlayerGui.Rebirth.MainFrame.ProgressBar.ProgressFill

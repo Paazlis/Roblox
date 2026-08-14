3e60ec4e72a4754021ce769626e20659ea766715
Paazlis <paazlismaswa@gmail.com>
2026-08-14 09:32:33 +0700

    Drop Balls For Brainrots V3 Completed 07-03-2026

--- Mods/Games/139992937215031.lua
+++ Mods/Games/139992937215031.lua
@@ -1,10 +1,10 @@
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/refs/heads/main/Packages/Sampluy/init.luau"))()
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/main/Packages/Sampluy/init.luau"))()
 
 local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
 local Players = Services.Players
 
 local LocalPlayer = Players.LocalPlayer
-local PlayerGui = LocalPlayer.PlayerGui
+local PlayerGui = LocalPlayer:FindFirstChildOfClass("PlayerGui")
 
 local DropEnabled,RebirthEnabled,CollectCashEnabled=false,false,false
 

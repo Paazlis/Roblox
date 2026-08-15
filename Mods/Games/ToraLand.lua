5db80edcf6222a2516a4339f9a7279b5f0d74b97
Paazlis <paazlismaswa@gmail.com>
2026-08-16 05:16:14 +0700

    Create ToraLand.lua

---
+++ Mods/Games/ToraLand.lua
@@ -0,0 +1,6 @@
+game:GetService("Players").LocalPlayer.PlayerGui.BalapKarungMeter.Meter.Content.Needle
+game:GetService("Players").LocalPlayer.PlayerGui.BalapKarungMeter.Meter.Content.Zone
+
+
+game:GetService("Players").LocalPlayer.PlayerGui.TarikTambangMeter.Meter.Content.Needle
+game:GetService("Players").LocalPlayer.PlayerGui.TarikTambangMeter.Meter.Content.Zone

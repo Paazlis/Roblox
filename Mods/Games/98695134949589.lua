86215361172696b5a217ff215c9f09a31d1e67b3
Paazlis <paazlismaswa@gmail.com>
2026-08-17 02:36:34 +0700

    +1 Follower Per Click V9 Completed 07-29-2026

--- Mods/Games/98695134949589.lua
+++ Mods/Games/98695134949589.lua
@@ -11,7 +11,7 @@ local Packets = {
 	["Click"] = ReplicatedStorage:QueryDescendants("#Remotes > #Events > #ClickRemote")[1]
 }
 local Enableds, Connections = {["Click"] = false, ["Upgrade"] = false, ["Rebirth"] = false}, {}
-local UpgradeTypes, UpgradeActives, UpgradeInfos = {}, {}, {}
+local UpgradeTypes, UpgradeActives, UpgradeInfos = {}, {["AllEnabled"] = true}, {}
 
 local RebirthFrame, RebirthFill, RebirthButton = PlayerGui:QueryDescendants("#MainGui > #RebirthMenu")[1], nil, nil
 if RebirthFrame then
@@ -61,6 +61,7 @@ local function IsFillFull(fill)
 end
 
 local function HandleClick()
+	if not Enableds.Click then return end
 	task.spawn(function()
 		while Enableds.Click do
 			Packets.Click:FireServer()
@@ -70,19 +71,19 @@ local function HandleClick()
 end
 
 local function HandleUpgrade()
+	if not Enableds.Upgrade then return end
 	task.spawn(function()
 		while Enableds.Upgrade do
 			for mode, active in pairs(UpgradeActives) do
-				task.wait()
+				task.wait(0.1)
 				if not Enableds.Upgrade then break end
+				if mode == "AllEnabled" then continue end
+				if UpgradeActives.AllEnabled then active = true end
 				if not active then continue end
-				
 				local upgradeStats = UpgradeInfos[mode]
 				if not upgradeStats then continue end
-				
 				local upgradeButton = upgradeStats.UpgradeButton
 				if not upgradeButton then continue end
-				
 				FireButton(upgradeButton)
 			end
 			task.wait(0.5)
@@ -90,19 +91,20 @@ local function HandleUpgrade()
 	end)
 end
 
+local function FireRebirth()
+    if Enableds.Rebirth and IsFillFull(RebirthFill) then
+		FireButton(RebirthButton)
+	end
+end
+
 local function HandleRebirth()
-	Connections.Rebirth = RebirthFill:GetPropertyChangedSignal("Size"):Connect(function()
-		if Enableds.Rebirth and IsFillFull(RebirthFill) then
-			FireButton(RebirthButton)
-		end
-	end)
-	
+	if Connections.Rebirth then Connections.Rebirth:Disconnect() Connections.Rebirth = nil end
+    if not Enableds.Rebirth then return end
+	Connections.Rebirth = RebirthFill:GetPropertyChangedSignal("Size"):Connect(FireRebirth)
 	if Enableds.Rebirth then
 		task.spawn(function()
 			while Enableds.Rebirth do
-				if IsFillFull(RebirthFill) then
-					FireButton(RebirthButton)
-				end
+				FireRebirth()
 				task.wait(0.5)
 			end
 		end)
@@ -115,7 +117,6 @@ local Window = UI:CreateWindow({
 		for key, enabled in pairs(Enableds) do
 			Enableds[key] = false
 		end
-
 		for key, connection in pairs(Connections) do
 			if connection then
 				connection:Disconnect()
@@ -130,22 +131,21 @@ Window:AddToggle({
 	Flag = "click_enabled",
 	Callback = function(value)
 		Enableds.Click = value
-		if value then
-			HandleClick()
-		end
+		HandleClick()
 	end
 })
 
 Window:AddDropdown({
-	Text = "Upgrade Type",
+	Text = "Upgrade Type (Empty = All)",
 	Options = #UpgradeTypes > 0 and UpgradeTypes or {"No Upgrade Type"},
-	Option = UpgradeTypes[1],
+	Option = nil,
 	MultipleOptions = true,
 	Flag = "upgrade_options",
 	Callback = function(option)
 		for _, mode in ipairs(UpgradeTypes) do
 			UpgradeActives[mode] = table.find(option, mode) ~= nil and true or false
-		end 
+		end
+		UpgradeActives.AllEnabled = #option <= 0
 	end
 })
 
@@ -155,9 +155,7 @@ Window:AddToggle({
 	Flag = "upgrade_enabled",
 	Callback = function(value)
 		Enableds.Upgrade = value
-		if value  then
-			HandleUpgrade()
-		end
+		HandleUpgrade()
 	end
 })
 
@@ -167,10 +165,7 @@ Window:AddToggle({
 	Flag = "rebirth_enabled",
 	Callback = function(value)
 		Enableds.Rebirth = value
-		if Connections.Rebirth then Connections.Rebirth:Disconnect() Connections.Rebirth = nil end
-		if value then
-			HandleRebirth()
-		end
+		HandleRebirth()
 	end
 })
 

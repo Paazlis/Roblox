13243aa9d10c7d6540e7afa256249550eaae5313
Paazlis <paazlismaswa@gmail.com>
2026-08-19 02:46:30 +0700

    +1 Monkey Escape 08-19-2026

---
+++ Mods/Games/91701030914075.lua
@@ -0,0 +1,22 @@
+-- Rebirth --
+game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.Menus.Rebirth.Container.Main
+game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.Menus.Rebirth.Container.Main.RebirthBtn
+game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.Menus.Rebirth.Container.Main.RebirthBar.ProgressLabel.Text == "20 / 20"
+
+local Event = game:GetService("ReplicatedStorage").Shared.Remotes.RequestRebirth
+Event:InvokeServer()
+
+-- Wins Farm --
+workspace.Map.Stages
+ workspace.Map.Stages.Stage3.Pad.Free.Pad
+Start Point in Player.RespawnPoint
+workspace.Map.Stages.Stage3.Barrier.CanCollide
+workspace:GetChildren()[49] -- Charater and EnemyId 
+------
+
+game:GetService("ReplicatedStorage").Client.DataController
+.observe("CurrentWorld",callback)
+
+
+
+

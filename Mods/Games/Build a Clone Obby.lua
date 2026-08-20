6562cfccd7ded001407cf0852e4fdefe1ab6c232
Paazlis <paazlismaswa@gmail.com>
2026-08-20 14:53:28 +0700

    Create Build a Clone Obby.lua

---
+++ Mods/Games/Build a Clone Obby.lua
@@ -0,0 +1,8 @@
+game:GetService("Players").LocalPlayer.PlayerGui.GUI.Frames.Rebirth.Background.Frame.Rebirth
+game:GetService("Players").LocalPlayer.PlayerGui.GUI.Frames.Rebirth.Background.Frame.Bar.Progress
+
+workspace.PlayerPlots.Plot_10952058440
+workspace.PlayerPlots.Plot_10952058440.Obbies
+workspace.PlayerPlots.Plot_10952058440.Obbies.StarterObby.CharacterSlots
+workspace.PlayerPlots.Plot_10952058440.Obbies.StarterObby.CharacterSlots.Slot1.CollectTouch
+workspace.PlayerPlots.Plot_10952058440.Obbies.ColorfulObby.CharacterSlots.Slot1.CollectTouch

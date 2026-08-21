b8230075a71e8ea5fdcb874ca6af2baf3dcac24a
Paazlis <paazlismaswa@gmail.com>
2026-08-21 13:41:40 +0700

    Save Your Cat V1 08-21-2026

---
+++ Mods/Games/119427438574114.lua
@@ -0,0 +1,13 @@
+-- Use for Wins Pad Select --
+workspace.StageButtons:GetChildren()[2]
+workspace.StageButtons.StageButton.TriggerPart 
+workspace.SeedButtonTPs
+
+-- Auto Build --
+ workspace.TycoonButtons.TycoonButton23.TriggerPart -- Color: 46, 204, 64
+
+-- Auto Rebirth --
+game:GetService("Players").LocalPlayer.PlayerGui.HudGui.Rebirth
+game:GetService("Players").LocalPlayer.PlayerGui.HudGui.Rebirth.ProgressBar.Bar
+-- game:GetService("Players").LocalPlayer.PlayerGui.HudGui.Rebirth.Rebirth
+game:GetService("ReplicatedStorage").Remotes.Rebirth

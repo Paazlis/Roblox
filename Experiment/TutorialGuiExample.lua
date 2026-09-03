e98a3920df04eb23add11574a7e9c5649abdc94a
Paazlis <paazlismaswa@gmail.com>
2026-09-04 01:14:45 +0700

    Update TutorialGuiExample.lua

--- Experiment/TutorialGuiExample.lua
+++ Experiment/TutorialGuiExample.lua
@@ -2,6 +2,7 @@ local e,frRGB,UD2,V2=Instance.new,Color3.fromRGB,UDim2.new,Vector2.new
 local V={
 ['1']=e('Model'),['2']=e('Model'),['3']=e('ScreenGui'),['4']=e('Frame'),['5']=e('Frame'),['6']=e('UIStroke'),['7']=e('ImageLabel'),['8']=e('UIAspectRatioConstraint'),['9']=e('UIScale')
 }
+
 V['1'].Name='InstalModel';V['1'].Parent=workspace;
 V['2'].Name='Gui';V['2'].Parent=V['1'];
 V['3'].Name='TutorialHilightMain';V['3'].Parent=V['2'];V['3'].DisplayOrder=125;V['3'].ResetOnSpawn=false;
@@ -11,3 +12,10 @@ V['6'].ApplyStrokeMode=Enum.ApplyStrokeMode.Border;V['6'].Thickness=100000;V['6'
 V['7'].Name='PointArrow';V['7'].Parent=V['5'];V['7'].BackgroundColor3=frRGB(255,255,255);V['7'].BackgroundTransparency=1;V['7'].BorderColor3=frRGB(0,0,0);V['7'].BorderSizePixel=0;V['7'].Position=UD2(0.263,0,1.147,0);V['7'].Size=UD2(0.726,0,2.054,0);V['7'].ZIndex=100;V['7'].Image="rbxassetid://127206568776222";
 V['8'].Parent=V['7'];V['8'].AspectRatio=1.01;
 V['9'].Parent=V['5'];V['9'].Scale=1.06;
+
+TutorialGui.TutorialFrame.Main
+TutorialGui.TutorialFrame.Main.UIScale -- zoom in/out loop forever until stop with function cleaup
+
+-- Question: 
+saat fungsi 
+ini terpanggil TargetGui(TutorialGui.TutorialFrame.Main)

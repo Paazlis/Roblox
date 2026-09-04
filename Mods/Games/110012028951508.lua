00c6c1e8a49c79699e9a1ebfb6fde7e192f629c5
Paazlis <paazlismaswa@gmail.com>
2026-09-05 04:19:42 +0700

    Army vs Rebels Tycoon V1 09-05-2026 (ASAP)

---
+++ Mods/Games/110012028951508.lua
@@ -0,0 +1,13 @@
+workspace.Tycoons["\005"].Buttons.Dropper2.Touch.Color == 26, 255, 0
+workspace.Tycoons["\005"].Buttons
+
+workspace.Tycoons
+workspace.Tycoons["\005"].Persistent.TycoonPlayerIconBillboard.BillboardGui.ImageLabel.Image == UserId
+
+
+
+workspace.Tycoons["\005"].CoreBuild.Collector.Collect
+
+
+game:GetService("Players").LocalPlayer.PlayerGui.RebirthGui.Frame.Buttons.Rebirth
+game:GetService("Players").LocalPlayer.PlayerGui.RebirthGui.Frame.Buttons.Rebirth.TextLabel -- not "left"
